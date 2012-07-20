ac1ddb1149ee1175f96d3f77f3bd6bf9390fc81c
Werner Lane <laneboysrc@gmail.com>
2012-07-20 10:14:21 +0800

    Separate light table into separate file.
    This allows us to use the same file for different RC models without
    having to change the source code.

---
+++ firmware/light-table-dingo.asm
@@ -0,0 +1,93 @@
+    TITLE       Light tables for the Axial SCX10 Dingo
+    RADIX       dec
+
+    #include    <p16f628a.inc>
+
+    GLOBAL local_light_table
+    GLOBAL slave_light_table
+    GLOBAL slave_light_half_table
+
+
+    ORG 0x010
+
+;============================================================================
+local_light_table
+    addwf   PCL, f
+
+            ; +------- OUT7     
+            ; |+------ OUT6 
+            ; ||+----- OUT5 
+            ; |||+---- OUT4
+            ; ||||+--- OUT3
+            ; |||||+-- OUT2
+            ; ||||||+- OUT1
+            ; |||||||+ OUT0
+    dt      b'00000001'     ; Stand lights
+    dt      b'00000011'     ; Head lights
+    dt      b'00000111'     ; Fog lights
+    dt      b'00001111'     ; High beam
+    dt      b'00010000'     ; Brake lights
+    dt      b'00100000'     ; Reverse lights
+    dt      b'10000000'     ; Indicator left
+    dt      b'01000000'     ; Indicator right
+    dt      b'11000000'     ; Hazard lights
+
+    IF ((HIGH ($)) != (HIGH (local_light_table)))
+        ERROR "local_light_table CROSSES PAGE BOUNDARY!"
+    ENDIF
+    
+
+;============================================================================
+slave_light_table
+    addwf   PCL, f
+
+            ; +------- (not used, must be 0!)     
+            ; |+------ OUT6 
+            ; ||+----- OUT5 
+            ; |||+---- OUT4
+            ; ||||+--- OUT3
+            ; |||||+-- OUT2
+            ; ||||||+- OUT1
+            ; |||||||+ OUT0
+    dt      b'00000001'     ; Stand lights
+    dt      b'00000011'     ; Head lights
+    dt      b'00000111'     ; Fog lights
+    dt      b'00001111'     ; High beam
+    dt      b'00010000'     ; Brake lights
+    dt      b'00000000'     ; Reverse lights
+    dt      b'00000000'     ; Indicator left
+    dt      b'00000000'     ; Indicator right
+    dt      b'00000000'     ; Hazard lights
+
+    IF ((HIGH ($)) != (HIGH (slave_light_table)))
+        ERROR "slave_light_table CROSSES PAGE BOUNDARY!"
+    ENDIF
+
+
+;============================================================================
+slave_light_half_table
+    addwf   PCL, f
+
+            ; +------- (not used, must be 0!)      
+            ; |+------ OUT6 
+            ; ||+----- OUT5 
+            ; |||+---- OUT4
+            ; ||||+--- OUT3
+            ; |||||+-- OUT2
+            ; ||||||+- OUT1
+            ; |||||||+ OUT0
+    dt      b'00010000'     ; Stand lights
+    dt      b'00010000'     ; Head lights
+    dt      b'00010000'     ; Fog lights
+    dt      b'00010000'     ; High beam
+    dt      b'00000000'     ; Brake lights
+    dt      b'00000000'     ; Reverse lights
+    dt      b'00000000'     ; Indicator left
+    dt      b'00000000'     ; Indicator right
+    dt      b'00000000'     ; Hazard lights
+
+    IF ((HIGH ($)) != (HIGH (slave_light_half_table)))
+        ERROR "slave_light_half_table CROSSES PAGE BOUNDARY!"
+    ENDIF
+
+    END
--- firmware/rc-light-controller.asm
+++ firmware/rc-light-controller.asm
@@ -204,6 +204,11 @@ swap_x_y    macro   x, y
 
 
 ;******************************************************************************
+; Relocatable code section
+;******************************************************************************
+    CODE
+
+;******************************************************************************
 ;******************************************************************************
 ;******************************************************************************
 ;
@@ -212,6 +217,10 @@ swap_x_y    macro   x, y
 ;******************************************************************************
 ;******************************************************************************
 ;******************************************************************************
+    EXTERN local_light_table
+    EXTERN slave_light_table
+    EXTERN slave_light_half_table
+
 light_table
     ; d0 indicates which light table we request:
     ;   0: local
@@ -221,85 +230,7 @@ light_table
     goto    slave_light_table
     btfsc   d0, 1
     goto    slave_light_half_table
-
-
-local_light_table
-    addwf   PCL, f
-
-            ; +------- OUT7     
-            ; |+------ OUT6 
-            ; ||+----- OUT5 
-            ; |||+---- OUT4
-            ; ||||+--- OUT3
-            ; |||||+-- OUT2
-            ; ||||||+- OUT1
-            ; |||||||+ OUT0
-    dt      b'00000001'     ; Stand lights
-    dt      b'00000011'     ; Head lights
-    dt      b'00000111'     ; Fog lights
-    dt      b'00001111'     ; High beam
-    dt      b'00010000'     ; Brake lights
-    dt      b'00100000'     ; Reverse lights
-    dt      b'10000000'     ; Indicator left
-    dt      b'01000000'     ; Indicator right
-    dt      b'11000000'     ; Hazard lights
-
-    IF ((HIGH ($)) != (HIGH (local_light_table)))
-        ERROR "local_light_table CROSSES PAGE BOUNDARY!"
-    ENDIF
-    
-
-slave_light_table
-    addwf   PCL, f
-
-            ; +------- (not used, must be 0!)     
-            ; |+------ OUT6 
-            ; ||+----- OUT5 
-            ; |||+---- OUT4
-            ; ||||+--- OUT3
-            ; |||||+-- OUT2
-            ; ||||||+- OUT1
-            ; |||||||+ OUT0
-    dt      b'00000001'     ; Stand lights
-    dt      b'00000011'     ; Head lights
-    dt      b'00000111'     ; Fog lights
-    dt      b'00001111'     ; High beam
-    dt      b'00010000'     ; Brake lights
-    dt      b'00000000'     ; Reverse lights
-    dt      b'00000000'     ; Indicator left
-    dt      b'00000000'     ; Indicator right
-    dt      b'00000000'     ; Hazard lights
-
-    IF ((HIGH ($)) != (HIGH (slave_light_table)))
-        ERROR "slave_light_table CROSSES PAGE BOUNDARY!"
-    ENDIF
-
-
-slave_light_half_table
-    addwf   PCL, f
-
-            ; +------- (not used, must be 0!)      
-            ; |+------ OUT6 
-            ; ||+----- OUT5 
-            ; |||+---- OUT4
-            ; ||||+--- OUT3
-            ; |||||+-- OUT2
-            ; ||||||+- OUT1
-            ; |||||||+ OUT0
-    dt      b'00010000'     ; Stand lights
-    dt      b'00010000'     ; Head lights
-    dt      b'00010000'     ; Fog lights
-    dt      b'00010000'     ; High beam
-    dt      b'00000000'     ; Brake lights
-    dt      b'00000000'     ; Reverse lights
-    dt      b'00000000'     ; Indicator left
-    dt      b'00000000'     ; Indicator right
-    dt      b'00000000'     ; Hazard lights
-
-    IF ((HIGH ($)) != (HIGH (slave_light_half_table)))
-        ERROR "slave_light_half_table CROSSES PAGE BOUNDARY!"
-    ENDIF
-
+    goto    local_light_table
 
 
 ;******************************************************************************
