0978d2b35c6ee026ceaac2f47422d06f1b243d2b
LANE Boys RC <laneboysrc@gmail.com>
2014-07-21 19:15:48 +0800

    Add HSL to RGB code

--- firmware/ws2812/test-ws2812-12f1840.c
+++ firmware/ws2812/test-ws2812-12f1840.c
@@ -56,6 +56,25 @@ static const uint8_t gamma6[] = {
     126,133,140,147,155,163,171,179,188,197,206,215,225,234,245,255
 };
 
+static const uint8_t dim_curve[] = {
+    0,   1,   1,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   3,   3,   3,
+    3,   3,   3,   3,   3,   3,   3,   4,   4,   4,   4,   4,   4,   4,   4,   4,
+    4,   4,   4,   5,   5,   5,   5,   5,   5,   5,   5,   5,   5,   6,   6,   6,
+    6,   6,   6,   6,   6,   7,   7,   7,   7,   7,   7,   7,   8,   8,   8,   8,
+    8,   8,   9,   9,   9,   9,   9,   9,   10,  10,  10,  10,  10,  11,  11,  11,
+    11,  11,  12,  12,  12,  12,  12,  13,  13,  13,  13,  14,  14,  14,  14,  15,
+    15,  15,  16,  16,  16,  16,  17,  17,  17,  18,  18,  18,  19,  19,  19,  20,
+    20,  20,  21,  21,  22,  22,  22,  23,  23,  24,  24,  25,  25,  25,  26,  26,
+    27,  27,  28,  28,  29,  29,  30,  30,  31,  32,  32,  33,  33,  34,  35,  35,
+    36,  36,  37,  38,  38,  39,  40,  40,  41,  42,  43,  43,  44,  45,  46,  47,
+    48,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,
+    63,  64,  65,  66,  68,  69,  70,  71,  73,  74,  75,  76,  78,  79,  81,  82,
+    83,  85,  86,  88,  90,  91,  93,  94,  96,  98,  99,  101, 103, 105, 107, 109,
+    110, 112, 114, 116, 118, 121, 123, 125, 127, 129, 132, 134, 136, 139, 141, 144,
+    146, 149, 151, 154, 157, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 190,
+    193, 196, 200, 203, 207, 211, 214, 218, 222, 226, 230, 234, 238, 242, 248, 255,
+};
+
 
 /*****************************************************************************
  Init_hardware()
@@ -171,6 +190,75 @@ WS2812_send_loop:
     __endasm;
 }
 
+uint8_t color_r;
+uint8_t color_g;
+uint8_t color_b;
+
+void getRGB(uint16_t hue, uint8_t sat, uint8_t val) {
+  /* convert hue, saturation and brightness ( HSB/HSV ) to RGB
+     The dim_curve is used only on brightness/value and on saturation (inverted).
+     This looks the most natural.      
+  */
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint16_t base;
+ 
+  val = dim_curve[val];
+  sat = 255 - dim_curve[255 - sat];
+ 
+ 
+  if (sat == 0) { // Acromatic color (gray). Hue doesn't mind.
+    color_r = val;
+    color_g = val;
+    color_b = val;  
+  } else  {
+ 
+    base = ((255 - sat) * val) >> 8;
+ 
+    switch(hue/60) {
+    case 0:
+        r = val;
+        g = (((val-base)*hue)/60)+base;
+        b = base;
+    break;
+ 
+    case 1:
+        r = (((val-base)*(60-(hue%60)))/60)+base;
+        g = val;
+        b = base;
+    break;
+ 
+    case 2:
+        r = base;
+        g = val;
+        b = (((val-base)*(hue%60))/60)+base;
+    break;
+ 
+    case 3:
+        r = base;
+        g = (((val-base)*(60-(hue%60)))/60)+base;
+        b = val;
+    break;
+ 
+    case 4:
+        r = (((val-base)*(hue%60))/60)+base;
+        g = base;
+        b = val;
+    break;
+ 
+    case 5:
+        r = val;
+        g = base;
+        b = (((val-base)*(60-(hue%60)))/60)+base;
+    break;
+    }
+ 
+    color_r=r;
+    color_g=g;
+    color_b=b;
+  }  
+}
 
 /*****************************************************************************
  main()
@@ -179,25 +267,51 @@ WS2812_send_loop:
  ****************************************************************************/
 void main(void) {
     uint16_t loop_counter;
-    uint8_t i;
-    uint8_t value;
-    
+    uint16_t h;
+    uint8_t s;
+    uint8_t v;
+    uint8_t up;
+
     Init_hardware();
-    
-    value = 1;
+
+    up = 1;    
+    h = 25;
+    s = 255;
+    v = 127;
     
     while (1) {
-        for (i = 0; i < sizeof(led_data) ; i++) {
-            led_data[i] = gamma4[value & 0x0f];
+        getRGB(h, s, v);
+        led_data[0] = color_r;
+        led_data[1] = color_g;
+        led_data[2] = color_b;
+
+        if (up) {
+            if (v == 255) {
+                up = 0;
+            }
+            else {
+                ++v;
+            }
         }
-        value <<= 1;
-        if (value == 0) {
-            value = 1;
+        else {
+            if (v == 0) {
+                up = 1;
+            }
+            else {
+                --v;
+            }
         }
+
+#if 0
+        ++h;
+        if (h >= 360) {
+            h = 0;
+        }
+#endif        
 
         WS2812_send();
 
-        for (loop_counter = 0; loop_counter < 1000; loop_counter++) {
+        for (loop_counter = 0; loop_counter < 2000; loop_counter++) {
             WREG = loop_counter & 0xff;
         }
     }
