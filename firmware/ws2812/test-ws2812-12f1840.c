4c31f433dbd090e52362a3c29d6241a9665ad938
Werner Lane <laneboysrc@gmail.com>
2014-07-21 14:08:07 +0800

    Improve timing and send loop

--- firmware/ws2812/test-ws2812-12f1840.c
+++ firmware/ws2812/test-ws2812-12f1840.c
@@ -62,48 +62,94 @@ static void Init_hardware(void) {
 
 /*****************************************************************************
 
- A "low" is a pulse duration of ~350ns, a "high" ~700ns.
- At 32 MHz we have an instruction cycle of 125ns, which means a low is
- ~3 instructions, and a high ~6 instructions.
- 
- According to source on the internet the low cycle of the pulse is not 
- so timing critical.
+ According to:  
+    http://cpldcpu.wordpress.com/2014/01/14/light_ws2812-library-v2-0-part-i-understanding-the-ws2812/
+
+ - A reset is issued as early as at 9 µs, contrary to the 50 µs mentioned in 
+   the data sheet. Longer delays between transmissions should be avoided.
+
+ - The cycle time of a bit should be at least 1.25 µs, the value given in the 
+   data sheet, and at most ~9 µs, the shortest time for a reset.
+
+ - A “0″ can be encoded with a pulse as short as 62.5 ns, but should not be 
+   longer than ~500 ns (maximum on WS2812).
+
+ - A “1″ can be encoded with pulses almost as long as the total cycle time, 
+   but it should not be shorter than ~625 ns (minimum on WS2812B). 
  
  ****************************************************************************/
-uint8_t count;
-uint8_t ws2812_data;
-void WS2812_send(uint8_t data) {
+// These variables are local to WS2812_send, but they had to be moved to 
+// global scope because the compiler optimized them away...
+static uint8_t ws2812_count;
+static uint8_t ws2812_data;
+void WS2812_send_byte(uint8_t data) {
 
     ws2812_data = data;
-    count = 8;
+    ws2812_count = 8;
     
     __asm
     BANKSEL _ws2812_data
     movfw   _ws2812_data
-    BANKSEL _count
+    BANKSEL _ws2812_count
 
-WS2812_send_loop:    
+WS2812_send_byte_loop:    
     btfsc   WREG, 7
-    goto    WS2812_send_loop_high
-    bsf     LATA, 2
-    nop
-    nop
-    bcf     LATA, 2
-    goto    WS2812_send_loop_end
+    goto    WS2812_send_byte_loop_high
     
-WS2812_send_loop_high:
-    bsf     LATA, 2
+WS2812_send_byte_loop_low:
+    bsf     LATA, 2                 ; LOW: 3 cycles = 375ns
+    nop
+    nop
+    bcf     LATA, 2                 
+    ; The loop takes 10 cycles from here to reach the next bit, which is 1000ns. 
+    ; This makes the total LOW pulse duration ~1375ns, which is fine according
+    ; to the info referenced above.
+    goto    WS2812_send_byte_loop_end
+    
+WS2812_send_byte_loop_high:
+    bsf     LATA, 2                 ; HIGH: 6 cycles = 750ns
     nop
     nop
     nop
     nop
     nop
     bcf     LATA, 2
+    ; From here the loop takes 7 cycles until the next bit, which is more than 
+    ; needed.
 
-WS2812_send_loop_end:
-    RLF     WREG
-    decfsz  _count, f
-    goto    WS2812_send_loop    
+WS2812_send_byte_loop_end:
+    rlf     WREG, f
+    decfsz  _ws2812_count, f
+    goto    WS2812_send_byte_loop    
+    __endasm;
+}
+
+
+/*****************************************************************************
+
+ By using assembler we can get the delay between two bytes from 5us down 
+ to 3us!
+ 
+ This means we can send data for 12 LEDs within 500us.
+ ****************************************************************************/
+uint8_t ws2812_array_count;
+void WS2812_send(void) {
+
+    ws2812_array_count = sizeof(led_data);
+
+    __asm
+    movlw	LOW _led_data
+    movwf   FSR0L
+    movlw	HIGH _led_data
+    movwf   FSR0H
+
+    BANKSEL _ws2812_array_count
+
+WS2812_send_loop:
+	moviw	FSR0++
+	call	_WS2812_send_byte    
+    decfsz  _ws2812_array_count, f
+    goto    WS2812_send_loop
     __endasm;
 }
 
@@ -121,22 +167,8 @@ void main(void) {
     Init_hardware();
     
     value = 1;
-
-    led_data[0] = 0xa5;
-    led_data[1] = 0xff;
-    led_data[2] = 0xaa;
-    led_data[3] = 0x20;
-    led_data[4] = 0x20;
-    led_data[5] = 0x20;
     
     while (1) {
-        for (i = 0; i < sizeof(led_data) ; i++) {
-            WS2812_send(led_data[i]);
-        }
-        for (loop_counter = 0; loop_counter < 1000; loop_counter++) {
-            WREG = loop_counter & 0xff;
-        }
-
         for (i = 0; i < sizeof(led_data) ; i++) {
             led_data[i] = value;
         }
@@ -144,7 +176,12 @@ void main(void) {
         if (value == 0) {
             value = 1;
         }
-        
+
+        WS2812_send();
+
+        for (loop_counter = 0; loop_counter < 1000; loop_counter++) {
+            WREG = loop_counter & 0xff;
+        }
     }
 }
 
