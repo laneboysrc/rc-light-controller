ed960d36001dba34e29c4c201b74aec9ebe627f8
Werner Lane <laneboysrc@gmail.com>
2014-09-24 14:22:23 +0800

    Implement light logic for Tobias' Jeep. Not tested.

--- firmware/lights-tobias-jeep.asm
+++ firmware/lights-tobias-jeep.asm
@@ -1,39 +1,67 @@
 ;******************************************************************************
 ;
-;   lights-sawback.asm
+;   lights-tobias-jeep.asm
 ;
 ;
 ;   This file contains the business logic to drive the LEDs for Tobias'
 ;   Axial SCX10 Jeep Wrangler.
 ;   Tobias contacted us via GMail.
 ;
+;   Requested button (CH3 emulation) functionality:
+;   When power on, the Fog lights and Parking (tail) lights would lit up.
+;   Also dimmed indicator front. (If possible)
+;
+;   First push of button:
+;       All above + Turning on front headlight.
+;
+;   Second push of button:
+;       All above + Extra lights;
+;
+;
 ;   The hardware is based on PIC16F1825 and TLC5940. No DC/DC converter is used.
-;   It is a master/slave system, similar to our Dingo but based on the 
+;   It is a master/slave system, similar to our Dingo but based on the
 ;   PIC16F1825.
 ;
 ;   The TLC5940 IREF is programmed with a 2000 Ohms resistor, which means
-;   the maximum LED current is 19.53 mA (39.06 / R). 
+;   the maximum LED current is 19.53 mA (39.06 / R).
 ;   Each adjustment step is 0.317 mA.
 ;
 ;
 ;   The following lights are available:
 ;
-;       OUT0    
-;       OUT1    
-;       OUT2    
-;       OUT3    
-;       OUT4    
-;       OUT5    
-;       OUT6    
-;       OUT7   
-;       OUT8    
-;       OUT9    
-;       OUT10   
-;       OUT11   
-;       OUT12   
-;       OUT13   
-;       OUT14   
-;       OUT15   
+;       Master OUT0     Fog front L
+;       Master OUT1     Fog front R
+;       Master OUT2     Extra front 1
+;       Master OUT3     Extra front 2
+;       Master OUT4     Extra front 3
+;       Master OUT5     Extra front 4
+;       Master OUT6     Tail/Brake rear L
+;       Master OUT7     Tail/Brake rear R
+;       Master OUT8
+;       Master OUT9
+;       Master OUT10
+;       Master OUT11
+;       Master OUT12
+;       Master OUT13
+;       Master OUT14
+;       Master OUT15
+;
+;       Slave OUT0      Main beam front L
+;       Slave OUT1      Main beam front R
+;       Slave OUT2      Indicator front L
+;       Slave OUT3      Indicator front R
+;       Slave OUT4      Tail/Brake rear L
+;       Slave OUT5      Tail/Brake rear R
+;       Slave OUT6      Indicator rear L
+;       Slave OUT7      Indicator rear R
+;       Slave OUT8      Reversing light rear L
+;       Slave OUT9      Reversing light rear R
+;       Slave OUT10
+;       Slave OUT11
+;       Slave OUT12
+;       Slave OUT13
+;       Slave OUT14
+;       Slave OUT15
 ;
 ;******************************************************************************
 ;
@@ -46,27 +74,27 @@
 
 ; Resistor determining the maximum LED current on the TLC5940; in Ohms
 ;###################
-#define R_IREF 2000                              
+#define R_IREF 2000
 ;###################
 
     #include    hw.tmp
-    
-    
+
+
     GLOBAL Init_lights
     GLOBAL Output_lights
 
-    
+
     ; Functions and variables imported from utils.asm
-    EXTERN Init_TLC5940    
+    EXTERN Init_TLC5940
     EXTERN TLC5940_send
     EXTERN UART_send_w
-    
+
     EXTERN xl
     EXTERN xh
     EXTERN temp
     EXTERN light_data
 
-    
+
     ; Functions and variables imported from master.asm
     EXTERN blink_mode
     EXTERN light_mode
@@ -85,10 +113,11 @@
 
 ; Bitfields in variable light_mode
 #define LIGHT_MODE_MAIN_BEAM 0      ; Main beam
+#define LIGHT_MODE_EXTRA 1          ; Extra lights beam
 
 ; Bitfields in variable drive_mode
-#define DRIVE_MODE_FORWARD 0 
-#define DRIVE_MODE_BRAKE 1 
+#define DRIVE_MODE_FORWARD 0
+#define DRIVE_MODE_BRAKE 1
 #define DRIVE_MODE_REVERSE 2
 
 ; Bitfields in variable setup_mode
@@ -106,19 +135,32 @@
 ; and send it to the slave
 #define STARTUP_MODE_NEUTRAL 4      ; Waiting before reading ST/TH neutral
 
-#define LED_MAIN_BEAM_L 0
-#define LED_MAIN_BEAM_R 1
-#define LED_INDICATOR_F_L 3    
-#define LED_INDICATOR_F_R 2
-#define LED_TAIL_L 12   
-#define LED_TAIL_R 13    
+#define LED_M_FOG_FRONT_L 0
+#define LED_M_FOG_FRONT_R 1
+#define LED_M_EXTRA_1 2
+#define LED_M_EXTRA_2 3
+#define LED_M_EXTRA_3 4
+#define LED_M_EXTRA_4 5
+#define LED_M_TAIL_BRAKE_L 6
+#define LED_M_TAIL_BRAKE_R 7
+
+#define LED_S_MAIN_BEAM_L 0
+#define LED_S_MAIN_BEAM_R 1
+#define LED_S_INDICATOR_F_L 2
+#define LED_S_INDICATOR_F_R 3
+#define LED_S_TAIL_BRAKE_L 4
+#define LED_S_TAIL_BRAKE_R 5
+#define LED_S_INDICATOR_R_L 6
+#define LED_S_INDICATOR_R_R 7
+#define LED_S_REVERSING_L 8
+#define LED_S_REVERSING_R 9
 
 
 ; We calculate the LED current per dot-correction step, so that later we can
 ; use the step size to determine the dot-correction value for a desired
 ; LED current.
 ;
-; The maximum current is 
+; The maximum current is
 ;       1.24V * 31.05 / R_IREF
 ; according to the TLC5940 datasheet. 1.24*31.05 is 39.06.
 ;
@@ -129,13 +171,17 @@
 #define VAL_FULL 63
 
 #define VAL_MAIN_BEAM VAL_FULL
+#define VAL_EXTRA VAL_FULL
+#define VAL_FOG VAL_FULL
 #define VAL_TAIL (4 * 1000 / uA_PER_STEP)
 #define VAL_BRAKE VAL_FULL
+#define VAL_REVERSING VAL_FULL
 #define VAL_INDICATOR_FRONT VAL_FULL
+#define VAL_INDICATOR_FRONT_DIM (4 * 1000 / uA_PER_STEP)
 #define VAL_INDICATOR_REAR VAL_FULL
 
 
-  
+
 ;******************************************************************************
 ; Relocatable variables section
 ;******************************************************************************
@@ -156,12 +202,14 @@ Init_lights
     call    Init_TLC5940
     call    Clear_light_data
 
-    ; Light up both front indicators until we receive the first command 
+    ; Light up both front indicators until we receive the first command
     ; from the UART
-    BANKSEL light_data
     movlw   VAL_INDICATOR_FRONT
-    movwf   light_data + LED_INDICATOR_F_R
-    movwf   light_data + LED_INDICATOR_F_L
+    BANKSEL light_data_slave
+    movwf   light_data + LED_S_INDICATOR_F_R
+    movwf   light_data + LED_S_INDICATOR_F_L
+
+output_lights_end
     call    TLC5940_send
     call    Slave_send
     return
@@ -172,6 +220,7 @@ Init_lights
 ;******************************************************************************
 Output_lights
     call    Clear_light_data
+    call    output_lights_always_on
 
     BANKSEL startup_mode
     movf    startup_mode, f
@@ -183,78 +232,22 @@ Output_lights
     ;----------
     ; Normal operation of lights goes here
     BANKSEL light_mode
-    movfw   light_mode
-    movwf   temp
-    btfsc   temp, LIGHT_MODE_MAIN_BEAM
-    call    output_lights_tail
-    btfsc   temp, LIGHT_MODE_MAIN_BEAM
+    btfsc   light_mode, LIGHT_MODE_MAIN_BEAM
     call    output_lights_main_beam
+    BANKSEL light_mode
+    btfsc   light_mode, LIGHT_MODE_EXTRA
+    call    output_lights_extra
 
     BANKSEL drive_mode
-    movfw   drive_mode
-    movwf   temp
-    btfsc   temp, DRIVE_MODE_BRAKE
+    btfsc   drive_mode, DRIVE_MODE_BRAKE
     call    output_lights_brake
 
+    BANKSEL drive_mode
+    btfsc   drive_mode, DRIVE_MODE_REVERSE
+    call    output_lights_reverse
 
-    ; Blinking the Sawback is very complicated due to combined tail, brake and
-    ; indicator lamp, same as our Tamiya XR311 where this code comes from.
-    ;
-    ; I decided on the following logic:
-    ;
-    ;                          BLINKFLAG    
-    ;                       on          off
-    ;  --------------------------------------
-    ;  Tail + Brake off     half        off   
-    ;  Tail                 half        off
-    ;  Brake                full        off
-    ;  Tail + Brake         full        half
-    ;
-    ; Note that an added complication is that for indicators we only want to
-    ; affect one side!
-
-    BANKSEL blink_mode
-    btfsc   blink_mode, BLINK_MODE_HAZARD
-    goto    _output_lights_blinking_hazard_is_active
-    btfsc   blink_mode, BLINK_MODE_INDICATOR_LEFT
-    goto    _output_lights_blinking_left_is_active
-    btfsc   blink_mode, BLINK_MODE_INDICATOR_RIGHT
-    goto    _output_lights_blinking_right_is_active
-    goto    _output_lights_blinking_end
-
-_output_lights_blinking_hazard_is_active
-    BANKSEL light_data
-    clrf    light_data + LED_TAIL_L
-    clrf    light_data + LED_TAIL_R
-
-    BANKSEL blink_mode
-    btfss   blink_mode, BLINK_MODE_BLINKFLAG
-    goto    _output_lights_indicators_off
-    goto    _output_lights_indicators_on
-
-_output_lights_blinking_left_is_active
-    BANKSEL light_data
-    clrf    light_data + LED_TAIL_L
-
-    BANKSEL blink_mode
-    btfss   blink_mode, BLINK_MODE_BLINKFLAG
-    goto    _output_lights_indicators_off
-    goto    _output_lights_indicators_on
-
-_output_lights_blinking_right_is_active
-    BANKSEL light_data
-    clrf    light_data + LED_TAIL_R
-
-    BANKSEL blink_mode
-    btfss   blink_mode, BLINK_MODE_BLINKFLAG
-    goto    _output_lights_indicators_off
-;   goto    _output_lights_indicators_on
-
-_output_lights_indicators_on
-    BANKSEL blink_mode
     movfw   blink_mode
     movwf   temp
-    BANKSEL light_data
     btfsc   temp, BLINK_MODE_HAZARD
     call    output_lights_indicator_left
     btfsc   temp, BLINK_MODE_HAZARD
@@ -264,54 +257,7 @@ _output_lights_indicators_on
     btfsc   temp, BLINK_MODE_INDICATOR_RIGHT
     call    output_lights_indicator_right
 
-    BANKSEL drive_mode
-    btfss   drive_mode, DRIVE_MODE_BRAKE
-    goto    _output_lights_combined_indicators_half
-
-    BANKSEL blink_mode
-    movfw   blink_mode
-    movwf   temp
-    movlw   VAL_INDICATOR_REAR
-    BANKSEL light_data
-    btfsc   temp, BLINK_MODE_HAZARD
-    movwf   light_data + LED_TAIL_L
-    btfsc   temp, BLINK_MODE_HAZARD
-    movwf   light_data + LED_TAIL_R
-    btfsc   temp, BLINK_MODE_INDICATOR_LEFT
-    movwf   light_data + LED_TAIL_L
-    btfsc   temp, BLINK_MODE_INDICATOR_RIGHT
-    movwf   light_data + LED_TAIL_R
-    goto    _output_lights_blinking_end    
-
-_output_lights_indicators_off
-    BANKSEL drive_mode
-    btfss   drive_mode, DRIVE_MODE_BRAKE
-    goto    _output_lights_blinking_end
-    btfss   light_mode, LIGHT_MODE_MAIN_BEAM
-    goto    _output_lights_blinking_end
-
-_output_lights_combined_indicators_half
-    BANKSEL blink_mode
-    movfw   blink_mode
-    movwf   temp
-    movlw   VAL_TAIL
-    BANKSEL light_data
-    btfsc   temp, BLINK_MODE_HAZARD
-    movwf   light_data + LED_TAIL_L
-    btfsc   temp, BLINK_MODE_HAZARD
-    movwf   light_data + LED_TAIL_R
-    btfsc   temp, BLINK_MODE_INDICATOR_LEFT
-    movwf   light_data + LED_TAIL_L
-    btfsc   temp, BLINK_MODE_INDICATOR_RIGHT
-    movwf   light_data + LED_TAIL_R
-;   goto    _output_lights_blinking_end
-
-_output_lights_blinking_end
-    
-output_lights_end
-    call    TLC5940_send
-    call    Slave_send
-    return
+    goto    output_lights_end
 
 
     ;----------
@@ -319,11 +265,11 @@ output_lights_end
 output_lights_startup
     btfss   startup_mode, STARTUP_MODE_NEUTRAL
     return
-    
-    BANKSEL light_data
+
     movlw   VAL_MAIN_BEAM
-    movwf   light_data + LED_MAIN_BEAM_L
-    movwf   light_data + LED_MAIN_BEAM_R
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_MAIN_BEAM_L
+    movwf   light_data_slave + LED_S_MAIN_BEAM_R
     goto    output_lights_end
 
 
@@ -336,10 +282,11 @@ output_lights_setup
     goto    output_lights_setup_left
     btfsc   setup_mode, SETUP_MODE_RIGHT
     goto    output_lights_setup_right
-    btfsc   setup_mode, SETUP_MODE_STEERING_REVERSE 
+
+    btfsc   setup_mode, SETUP_MODE_STEERING_REVERSE
     call    output_lights_indicator_left
     BANKSEL setup_mode  ; Since we do a "call" before we need to reset the bank!
-    btfsc   setup_mode, SETUP_MODE_THROTTLE_REVERSE 
+    btfsc   setup_mode, SETUP_MODE_THROTTLE_REVERSE
     call    output_lights_main_beam
     goto    output_lights_end
 
@@ -356,38 +303,80 @@ output_lights_setup_right
     call    output_lights_indicator_right
     goto    output_lights_end
 
-    
-output_lights_main_beam
+
+output_lights_always_on
+    ; Fog, tail and front indicators (dim)
+    movlw   VAL_FOG
     BANKSEL light_data
+    movwf   light_data + LED_M_FOG_FRONT_L
+    movwf   light_data + LED_M_FOG_FRONT_R
+
+    call    output_lights_tail
+
+    movlw   VAL_INDICATOR_FRONT_DIM
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_INDICATOR_F_L
+    movwf   light_data_slave + LED_S_INDICATOR_F_R
+
+    return
+
+output_lights_main_beam
     movlw   VAL_MAIN_BEAM
-    movwf   light_data + LED_MAIN_BEAM_L
-    movwf   light_data + LED_MAIN_BEAM_R
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_MAIN_BEAM_L
+    movwf   light_data_slave + LED_S_MAIN_BEAM_R
+    return
+
+output_lights_extra
+    movlw   VAL_EXTRA
+    BANKSEL light_data
+    movwf   light_data + LED_M_EXTRA_1
+    movwf   light_data + LED_M_EXTRA_2
+    movwf   light_data + LED_M_EXTRA_3
+    movwf   light_data + LED_M_EXTRA_4
     return
 
 output_lights_tail
-    BANKSEL light_data
     movlw   VAL_TAIL
-    movwf   light_data + LED_TAIL_L
-    movwf   light_data + LED_TAIL_R
+    BANKSEL light_data
+    movwf   light_data + LED_M_TAIL_BRAKE_L
+    movwf   light_data + LED_M_TAIL_BRAKE_R
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_TAIL_BRAKE_L
+    movwf   light_data_slave + LED_S_TAIL_BRAKE_R
     return
-    
+
 output_lights_brake
-    BANKSEL light_data
     movlw   VAL_BRAKE
-    movwf   light_data + LED_TAIL_L
-    movwf   light_data + LED_TAIL_R
+    BANKSEL light_data
+    movwf   light_data + LED_M_TAIL_BRAKE_L
+    movwf   light_data + LED_M_TAIL_BRAKE_R
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_TAIL_BRAKE_L
+    movwf   light_data_slave + LED_S_TAIL_BRAKE_R
     return
-    
+
+output_lights_reverse
+    movlw   VAL_REVERSING
+    BANKSEL light_data_slave
+    movwf   light_data_slave + LED_S_REVERSING_L
+    movwf   light_data_slave + LED_S_REVERSING_R
+    return
+
 output_lights_indicator_left
-    BANKSEL light_data
+    BANKSEL light_data_slave
     movlw   VAL_INDICATOR_FRONT
-    movwf   light_data + LED_INDICATOR_F_L
+    movwf   light_data_slave + LED_S_INDICATOR_F_L
+    movlw   VAL_INDICATOR_REAR
+    movwf   light_data_slave + LED_S_INDICATOR_R_L
     return
-    
+
 output_lights_indicator_right
-    BANKSEL light_data
+    BANKSEL light_data_slave
     movlw   VAL_INDICATOR_FRONT
-    movwf   light_data + LED_INDICATOR_F_R
+    movwf   light_data_slave + LED_S_INDICATOR_F_R
+    movlw   VAL_INDICATOR_REAR
+    movwf   light_data_slave + LED_S_INDICATOR_R_R
     return
 
 
@@ -405,10 +394,10 @@ Slave_send
     movlw   LOW light_data
     movwf   FSR0L
 
-    movlw   16          
+    movlw   16
     movwf   temp
 slave_send_loop
-    moviw   FSR0++    
+    moviw   FSR0++
     call    UART_send_w
     decfsz  temp, f
     goto    slave_send_loop
@@ -435,12 +424,12 @@ Clear_light_data
 clear_light_data_start
     movlw   16          ; There are 16 bytes in light_data / light_data_slave
     movwf   temp
-    clrw   
+    clrw
 clear_light_data_loop
-    movwi   FSR0++    
+    movwi   FSR0++
     decfsz  temp, f
     goto    clear_light_data_loop
 
     return
-    
+
     END
