1e9fd29b07b2889979c3e899168fc434f26987d9
LANE Boys RC <laneboysrc@gmail.com>
2014-09-24 21:07:31 +0800

    Work on the preprocessor-simulator

--- tools/preprocessor-simulator.py
+++ tools/preprocessor-simulator.py
@@ -1,88 +1,177 @@
-import time
+'''
+preprocessor-simulator.py
 
-### {{{ http://code.activestate.com/recipes/134892/ (r2)
-#class _Getch:
-#    """Gets a single character from standard input.  Does not echo to the
-#screen."""
-#    def __init__(self):
-#        try:
-#            self.impl = _GetchWindows()
-#        except ImportError:
-#            self.impl = _GetchUnix()
+Simulate a receiver with built-in preprocessor. This allow testing of the
+light controller functionality without hooking up a RC system.
 
-#    def __call__(self): return self.impl()
+A web browser is used for the user interface
+
+Author:         Werner Lane
+E-mail:         laneboysrc@gmail.com
+'''
+
+from __future__ import print_function
+
+import sys
+import argparse
+import serial
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from urlparse import urlparse, parse_qs
+
+HTML = '''\
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Preprocessor simulator - DIY RC Light Controller</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>Preprocessor simulator</h1>
+        <h2>DIY RC Light Controller</h2>
+        <div >
+            <input type="range" min="-100" max="100" step="1" name="st"
+                value="0" style="width:80%;" onchange="send('st', this.value);",
+                oninput="send('st', this.value);" />
+        </div>
+        <div>
+            Throttle
+        </div>
+        <div>
+            CH3 (AUX)
+        </div>
+
+        <div id="response">
+        </div>
+
+        <script>
+            function createXMLHttpRequest() {
+                try { return new XMLHttpRequest(); } catch(e) {}
+                try { return new ActiveXObject("Msxml2.XMLHTTP"); } catch (e) {}
+                return null;
+            }
+
+            function send(type, value) {
+                var params = type + "=" + value;
+
+                var xhr = createXMLHttpRequest();
+                xhr.onerror = function(evt) {
+                    msg = "ERR Unable to send XMLHttpRequest";
+                    document.getElementById("response").innerHTML = msg;
+                }
+
+                xhr.onload = function(evt) {
+                    document.getElementById("response").innerHTML = xhr.responseText;
+                }
+
+                xhr.open("GET", "/api?" + params, true);
+
+                //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.setRequestHeader("Connection", "close");
+
+                xhr.send(params);
+            }
+        </script>
+    </body>
+</html>
+'''
 
 
-#class _GetchUnix:
-#    def __init__(self):
-#        import tty, sys
+def parse_commandline():
+    ''' Simulate a receiver with built-in preprocessor '''
+    parser = argparse.ArgumentParser(
+        description="Simulate a receiver with built-in preprocessor.")
 
-#    def __call__(self):
-#        import sys, tty, termios
-#        fd = sys.stdin.fileno()
-#        old_settings = termios.tcgetattr(fd)
-#        try:
-#            tty.setraw(sys.stdin.fileno())
-#            ch = sys.stdin.read(1)
-#        finally:
-#            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-#        return ch
+    parser.add_argument("-b", "--baudrate", type=int, default=38400,
+        help='Baudrate to use. Default is 38400.')
 
+    parser.add_argument("-p", "--port", type=int, default=1234,
+        help='HTTP port for the web UI. Default is localhost:1234.')
 
-#class _GetchWindows:
-#    def __init__(self):
-#        import msvcrt
+    parser.add_argument("tty", nargs="?", default="/dev/ttyUSB0",
+        help="serial port to use. ")
 
-#    def __call__(self):
-#        import msvcrt
-#        return msvcrt.getch()
+    return parser.parse_args()
 
 
-#getch = _Getch()
-### end of http://code.activestate.com/recipes/134892/ }}}
+class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
+    ''' Request handler that implements our simple web based API '''
 
-try:
-    from msvcrt import kbhit, getch
-except ImportError:
-    import termios, fcntl, sys, os, tty
-    def kbhit():
-        fd = sys.stdin.fileno()
-        oldterm = termios.tcgetattr(fd)
-        newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-        termios.tcsetattr(fd, termios.TCSANOW, newattr)
-        oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+    def do_GET(self):
+        ''' GET request handler '''
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(HTML)
+
+        elif self.path.startswith('/api?'):
+            try:
+                query = parse_qs(urlparse(self.path).query, strict_parsing=True)
+
+            except ValueError:
+                self.send_response(400)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write("Bad querystring")
+
+            else:
+                response, content = self.server.preprocessor.api(query)
+                self.send_response(response)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(content)
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("Not found")
+        return
+
+    def do_POST(self):
+        ''' POST request handler '''
+        self.send_response(405)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write("POST not implemented")
+        return
+
+
+class PreprocessorApp(object):
+    ''' Simulate a RC receiver with preprocessor '''
+
+    def __init__(self):
+        self.args = parse_commandline()
+
         try:
-            while True:
-                try:
-                    c = sys.stdin.read(1)
-                    return c
-                except IOError:
-                    return False
-        finally:
-            termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-            fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+            self.uart = serial.Serial(self.args.tty, self.args.baudrate)
+        except serial.SerialException as error:
+            print("Unable to open port %s: %s" % (self.args.tty, error))
+            sys.exit(1)
 
-    def getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-            
-count = 0
-while True:
-    c = kbhit()
-    if c:
-        if ord(c) == 113:
-            break
-        print "%d" % (	ord(c), ) 
-    else:
-        time.sleep(20/1000)
-        print "\x08\x08%s" % ("-\|/"[count/10%4], ),
-        count += 1
-    
+        print("Simulating on {uart} at {baudrate} baud.".format(
+            uart=self.uart.port, baudrate=self.uart.baudrate))
+
+    def api(self, query):
+        ''' Web api handler '''
+        print(query)
+        return 200, "OK"
+
+    def run(self):
+        ''' Send the test patterns to the TLC5940 based slave '''
+        server = HTTPServer(('', self.args.port), CustomHTTPRequestHandler)
+        server.preprocessor = self
+        print("Please call up the user interface on localhost:{port}".format(
+            port=self.args.port))
+        server.serve_forever()
+
+
+if __name__ == '__main__':
+    try:
+        PreprocessorApp().run()
+    except KeyboardInterrupt:
+        print("")
+        sys.exit(0)
+
