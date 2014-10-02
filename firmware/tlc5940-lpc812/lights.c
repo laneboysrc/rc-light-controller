b528034d46aa8cde8015aaf20ea4bbdb21988646
LANE Boys RC <laneboysrc@gmail.com>
2014-10-02 19:15:27 +0800

    More refactoring

--- firmware/tlc5940-lpc812/lights.c
+++ firmware/tlc5940-lpc812/lights.c
@@ -93,7 +93,7 @@
 #include <globals.h>
 #include <uart0.h>
 
-#define MAX_LIGHT_SWITCH_POSITIONS 9
+#define mix_light_SWITCH_POSITIONS 9
 
 static uint8_t light_switch_position;
 static uint8_t tlc5940_light_data[16];
@@ -122,7 +122,7 @@ typedef uint8_t SINGLE_COLOR_LED_T;
 typedef struct {
     SINGLE_COLOR_LED_T always_on;
 
-    SINGLE_COLOR_LED_T light_switch_position[MAX_LIGHT_SWITCH_POSITIONS];
+    SINGLE_COLOR_LED_T light_switch_position[mix_light_SWITCH_POSITIONS];
 
     SINGLE_COLOR_LED_T tail_light;
     SINGLE_COLOR_LED_T brake_light;
@@ -176,17 +176,17 @@ static void send_light_data_to_tlc5940(void)
     volatile int i;
 
     // Wait for MSTIDLE
-    while (!(LPC_SPI0->STAT & (1u << 8)));
+    while (!(LPC_SPI0->STAT & (1 << 8)));
 
     for (i = 15; i >= 0; i--) {
         // Wait for TXRDY
-        while (!(LPC_SPI0->STAT & (1u << 1)));
+        while (!(LPC_SPI0->STAT & (1 << 1)));
 
         LPC_SPI0->TXDAT = tlc5940_light_data[i];
     }
 
     // Force END OF TRANSFER
-    LPC_SPI0->STAT = (1u << 7);
+    LPC_SPI0->STAT = (1 << 7);
 }
 
 
@@ -197,20 +197,20 @@ void init_lights(void)
     XLAT = 0;
 
     LPC_GPIO_PORT->DIR0 |=
-        (1u << 1) | (1u << 2) | (1u << 3) | (1u << 6) | (1u << 7);
+        (1 << 1) | (1 << 2) | (1 << 3) | (1 << 6) | (1 << 7);
 
     // Use 2 MHz SPI clock. 16 bytes take about 50 us to transmit.
     LPC_SPI0->DIV = (__SYSTEM_CLOCK / 2000000) - 1;
 
-    LPC_SPI0->CFG = (1u << 0) |          // Enable SPI0
-                    (1u << 2) |          // Master mode
+    LPC_SPI0->CFG = (1 << 0) |          // Enable SPI0
+                    (1 << 2) |          // Master mode
                     (0 << 3) |          // LSB First mode disabled
                     (0 << 4) |          // CPHA = 0
                     (0 << 5) |          // CPOL = 0
                     (0 << 8);           // SPOL = 0
 
-    LPC_SPI0->TXCTRL = (1u << 21) |      // set EOF
-                       (1u << 22) |      // RXIGNORE, otherwise SPI hangs until
+    LPC_SPI0->TXCTRL = (1 << 21) |      // set EOF
+                       (1 << 22) |      // RXIGNORE, otherwise SPI hangs until
                                         //   we read the data register
                        ((6 - 1) << 24); // 6 bit frames
 
@@ -259,7 +259,7 @@ void toggle_light_switch(void)
 }
 
 
-static void max_light(SINGLE_COLOR_LED_T *led, uint8_t value)
+static void mix_light(SINGLE_COLOR_LED_T *led, uint8_t value)
 {
     if (value > *led) {
         *led = value;
@@ -272,13 +272,13 @@ static void combined_tail_brake(
 {
     if (current_light->tail_light) {
         if (light_switch_position > 0) {
-            max_light(current_led, current_light->tail_light);
+            mix_light(current_led, current_light->tail_light);
         }
     }
 
     if (current_light->brake_light) {
         if (global_flags.braking) {
-            max_light(current_led, current_light->brake_light);
+            mix_light(current_led, current_light->brake_light);
         }
     }
 }
@@ -291,14 +291,14 @@ static void combined_tail_brake_indicators(
     SINGLE_COLOR_LED_T active_indicator = 0;
 
     if (global_flags.blink_indicator_left || global_flags.blink_hazard) {
-        max_light(&active_indicator, current_light->indicator_left);
+        mix_light(&active_indicator, current_light->indicator_left);
     }
     if (global_flags.blink_indicator_right) {
-        max_light(&active_indicator, current_light->indicator_right);
+        mix_light(&active_indicator, current_light->indicator_right);
     }
     if (global_flags.blink_hazard) {
-        max_light(&active_indicator, current_light->indicator_left);
-        max_light(&active_indicator, current_light->indicator_right);
+        mix_light(&active_indicator, current_light->indicator_left);
+        mix_light(&active_indicator, current_light->indicator_right);
     }
 
     if (active_indicator > 0) {
@@ -315,13 +315,13 @@ static void combined_tail_brake_indicators(
             // by tail and finally the indicator value
 
             if (global_flags.braking) {
-                max_light(current_led, current_light->brake_light);
+                mix_light(current_led, current_light->brake_light);
             }
             else if (light_switch_position > 0) {
-                max_light(current_led, current_light->tail_light);
+                mix_light(current_led, current_light->tail_light);
             }
             else {
-                max_light(current_led, active_indicator);
+                mix_light(current_led, active_indicator);
             }
         }
         else {
@@ -329,7 +329,7 @@ static void combined_tail_brake_indicators(
             // active
 
             if (light_switch_position > 0 && global_flags.braking) {
-                max_light(current_led, current_light->tail_light);
+                mix_light(current_led, current_light->tail_light);
             }
         }
     }
@@ -340,58 +340,60 @@ static void combined_tail_brake_indicators(
 }
 
 
+static void process_single_color_light(
+    TLC5940_CAR_LIGHT_T *current_light, SINGLE_COLOR_LED_T *current_led)
+{
+    *current_led = 0;
+
+    if (current_light->always_on) {
+        *current_led = current_light->always_on;
+    }
+
+    if (current_light->light_switch_position[light_switch_position] > 0) {
+        *current_led =
+            current_light->light_switch_position[light_switch_position];
+    }
+
+    if (current_light->reversing_light) {
+        if (global_flags.reversing) {
+            mix_light(current_led, current_light->reversing_light);
+        }
+    }
+
+    if (current_light->tail_light &&
+        current_light->brake_light &&
+        (current_light->indicator_left || current_light->indicator_right)) {
+        // Special case for combined tail / brake / indicators
+        combined_tail_brake_indicators(current_light, current_led);
+    }
+    else {
+        combined_tail_brake(current_light, current_led);
+
+        if (current_light->indicator_left) {
+            if (global_flags.blink_flag &&
+                (global_flags.blink_hazard ||
+                 global_flags.blink_indicator_left)) {
+                mix_light(current_led, current_light->indicator_left);
+            }
+        }
+
+        if (current_light->indicator_right) {
+            if (global_flags.blink_flag &&
+                (global_flags.blink_hazard ||
+                 global_flags.blink_indicator_right)) {
+                mix_light(current_led, current_light->indicator_right);
+            }
+        }
+    }
+}
+
+
 static void process_car_lights(void)
 {
     int i;
 
     for (i = 0; i < 16 ; i++) {
-        TLC5940_CAR_LIGHT_T *current_light;
-        SINGLE_COLOR_LED_T *current_led;
-
-        current_light = &local_leds[i];
-        current_led = &tlc5940_light_data[i];
-        *current_led = 0;
-
-        if (current_light->always_on) {
-            *current_led = current_light->always_on;
-        }
-
-        if (current_light->light_switch_position[light_switch_position] > 0) {
-            *current_led =
-                current_light->light_switch_position[light_switch_position];
-        }
-
-        if (current_light->reversing_light) {
-            if (global_flags.reversing) {
-                max_light(current_led, current_light->reversing_light);
-            }
-        }
-
-        if (current_light->tail_light &&
-            current_light->brake_light &&
-            (current_light->indicator_left || current_light->indicator_right)) {
-            // Special case for combined tail / brake / indicators
-            combined_tail_brake_indicators(current_light, current_led);
-        }
-        else {
-            combined_tail_brake(current_light, current_led);
-
-            if (current_light->indicator_left) {
-                if (global_flags.blink_flag &&
-                    (global_flags.blink_hazard ||
-                     global_flags.blink_indicator_left)) {
-                    max_light(current_led, current_light->indicator_left);
-                }
-            }
-
-            if (current_light->indicator_right) {
-                if (global_flags.blink_flag &&
-                    (global_flags.blink_hazard ||
-                     global_flags.blink_indicator_right)) {
-                    max_light(current_led, current_light->indicator_right);
-                }
-            }
-        }
+        process_single_color_light(&local_leds[i], &tlc5940_light_data[i]);
     }
 }
 
