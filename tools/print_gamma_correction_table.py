3a380e4074647371c632d255dedf11c5690d7b40
Werner Lane <laneboysrc@gmail.com>
2014-10-03 14:55:06 +0800

    Add gamma table to the project. Not used for now.

---
+++ tools/print_gamma_correction_table.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+'''
+Gamma correction for LEDs
+'''
+
+from __future__ import print_function
+
+def gamma_correction(level, gamma):
+    ''' Calculate gamma corrected 8-bit value '''
+    return int(255 * pow(float(level) / 255, gamma))
+
+def main():
+    ''' Main application '''
+    gamma = 2.2
+    level = 1
+    print('const uint8_t gamma_table[] = {')
+    while level < 256:
+        step = gamma_correction(level, gamma)
+        print("{step:d}, ".format(level=level, step=step), end='')
+        level += 1
+
+    print('\n};')
+
+main()
