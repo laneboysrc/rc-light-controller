8339cdc721cf3ba7577bc6635a5114c802d91e2d
LANE Boys RC <laneboysrc@gmail.com>
2014-10-04 09:26:57 +0800

    Proper rounding up/down above/below .5. Also make gamma a command line option

--- tools/print_gamma_correction_table.py
+++ tools/print_gamma_correction_table.py
@@ -4,19 +4,33 @@ Gamma correction for LEDs
 '''
 
 from __future__ import print_function
+import sys
+from decimal import Decimal, ROUND_HALF_UP
 
 def gamma_correction(level, gamma):
     ''' Calculate gamma corrected 8-bit value '''
-    return int(255 * pow(float(level) / 255, gamma))
+
+    return Decimal(255 * pow(float(level) / 255, gamma)).to_integral_value(
+        rounding=ROUND_HALF_UP)
 
 def main():
     ''' Main application '''
-    gamma = 2.2
-    level = 0
+
+    try:
+        gamma = float(sys.argv[1])
+
+    except ValueError:
+        print("usage: {} [gamma_as_float]".format(sys.argv[0]))
+        sys.exit(1)
+
+    except IndexError:
+        gamma = 2.2
+
     print('const uint8_t gamma_table[] = {')
-    while level < 256:
+
+    for level in range(256):
         step = gamma_correction(level, gamma)
-        print("{step:d}, ".format(level=level, step=step), end='')
+        print("{step:d}, ".format(level=level, step=int(step)), end='')
         level += 1
 
     print('\n};')
