cb0fe1de223b55f42d4eb9271d9d8d5f9df2dd6e
Werner Lane <wl-opensource@gmx.net>
2014-11-09 18:48:39 +0800

    Make JSLint pass

--- tools/configurator/src/class_manipulation.js
+++ tools/configurator/src/class_manipulation.js
@@ -6,12 +6,14 @@ function hasClass(ele, cls) {
 }
 
 function addClass(ele, cls) {
-    if (!this.hasClass(ele, cls)) ele.className += " " + cls;
+    if (!this.hasClass(ele, cls)) {
+        ele.className += " " + cls;
+    }
 }
 
 function removeClass(ele, cls) {
     if (hasClass(ele, cls)) {
         var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-        ele.className = ele.className.replace(reg,' ');
+        ele.className = ele.className.replace(reg, ' ');
     }
 }
