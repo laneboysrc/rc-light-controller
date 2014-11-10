3c18b64663e4c7b433a6e7561755d737f5192f1a
Werner Lane <laneboysrc@gmail.com>
2014-11-10 14:15:39 +0800

    Add no-compress task to generate an in-lined HTML for debugging

--- tools/configurator/gulpfile.js
+++ tools/configurator/gulpfile.js
@@ -1,13 +1,29 @@
+"use strict";
+/*jslint node: true*/
+
 var gulp = require("gulp");
 var inlinesource = require('gulp-inline-source');
 
-gulp.task('default', function() {
+
+gulp.task('default', function () {
     var options = {
         compress: true,
         swallowErrors: true,
     };
 
-   return gulp.src('./configurator.html')
+    return gulp.src('./configurator.html')
+        .pipe(inlinesource(options))
+        .pipe(gulp.dest("./build"));
+});
+
+
+gulp.task('no-compress', function () {
+    var options = {
+        compress: false,
+        swallowErrors: true,
+    };
+
+    return gulp.src('./configurator.html')
         .pipe(inlinesource(options))
         .pipe(gulp.dest("./build"));
 });
