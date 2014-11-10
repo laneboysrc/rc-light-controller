fe97e65cee1d8a2ee9f1960e0e555957a56de8da
Werner Lane <laneboysrc@gmail.com>
2014-11-10 11:12:16 +0800

    Fix missing comment

--- tools/text2js.py
+++ tools/text2js.py
@@ -28,7 +28,7 @@ Tool for creating a JavaScript string containing the content of a text file.''')
 
 
 def text2js(args):
-
+    ''' Dump all lines as JavaScript string '''
     print("var {} =".format(args.var_name[0]))
 
     for line in args.text_file[0]:
