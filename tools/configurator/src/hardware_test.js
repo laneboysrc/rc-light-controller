c4af9de8bb18e693c01a7b332f275f6c72f6a37b
Werner Lane <laneboysrc@gmail.com>
2014-12-08 14:22:38 +0800

    Fix test program configuration

--- tools/configurator/src/hardware_test.js
+++ tools/configurator/src/hardware_test.js
@@ -6,9 +6,8 @@ var hardware_test_configuration = {
     "slave_output": false,
     "preprocessor_output": false,
     "winch_output": false,
-    "steering_wheel_servo_output": false,
+    "steering_wheel_servo_output": true,
     "gearbox_servo_output": false,
-    "switched_light_output": true,
     "ch3_is_local_switch": false,
     "ch3_is_momentary": false,
     "auto_brake_lights_forward_enabled": true,
@@ -57,10 +56,10 @@ var hardware_test_configuration = {
       "weak_indicator_right": false,
       "always_on": 0,
       "light_switch_position0": 0,
-      "light_switch_position1": 0,
-      "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position1": 255,
+      "light_switch_position2": 255,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -90,10 +89,10 @@ var hardware_test_configuration = {
       "weak_indicator_right": false,
       "always_on": 0,
       "light_switch_position0": 0,
-      "light_switch_position1": 0,
-      "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position1": 255,
+      "light_switch_position2": 255,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -124,9 +123,9 @@ var hardware_test_configuration = {
       "always_on": 0,
       "light_switch_position0": 0,
       "light_switch_position1": 0,
-      "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position2": 255,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -157,9 +156,9 @@ var hardware_test_configuration = {
       "always_on": 0,
       "light_switch_position0": 0,
       "light_switch_position1": 0,
-      "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position2": 255,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -191,8 +190,8 @@ var hardware_test_configuration = {
       "light_switch_position0": 0,
       "light_switch_position1": 0,
       "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -224,8 +223,8 @@ var hardware_test_configuration = {
       "light_switch_position0": 0,
       "light_switch_position1": 0,
       "light_switch_position2": 0,
-      "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position3": 255,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -266,7 +265,7 @@ var hardware_test_configuration = {
       "tail_light": 0,
       "brake_light": 0,
       "reversing_light": 0,
-      "indicator_left": 0,
+      "indicator_left": 255,
       "indicator_right": 0
     },
     "7": {
@@ -300,7 +299,7 @@ var hardware_test_configuration = {
       "brake_light": 0,
       "reversing_light": 0,
       "indicator_left": 0,
-      "indicator_right": 0
+      "indicator_right": 255
     },
     "8": {
       "max_change_per_systick": 0,
@@ -329,8 +328,8 @@ var hardware_test_configuration = {
       "light_switch_position6": 0,
       "light_switch_position7": 0,
       "light_switch_position8": 0,
-      "tail_light": 0,
-      "brake_light": 0,
+      "tail_light": 84,
+      "brake_light": 255,
       "reversing_light": 0,
       "indicator_left": 0,
       "indicator_right": 0
@@ -362,8 +361,8 @@ var hardware_test_configuration = {
       "light_switch_position6": 0,
       "light_switch_position7": 0,
       "light_switch_position8": 0,
-      "tail_light": 0,
-      "brake_light": 0,
+      "tail_light": 84,
+      "brake_light": 255,
       "reversing_light": 0,
       "indicator_left": 0,
       "indicator_right": 0
@@ -397,7 +396,7 @@ var hardware_test_configuration = {
       "light_switch_position8": 0,
       "tail_light": 0,
       "brake_light": 0,
-      "reversing_light": 0,
+      "reversing_light": 255,
       "indicator_left": 0,
       "indicator_right": 0
     },
@@ -430,7 +429,7 @@ var hardware_test_configuration = {
       "light_switch_position8": 0,
       "tail_light": 0,
       "brake_light": 0,
-      "reversing_light": 0,
+      "reversing_light": 255,
       "indicator_left": 0,
       "indicator_right": 0
     },
@@ -464,7 +463,7 @@ var hardware_test_configuration = {
       "tail_light": 0,
       "brake_light": 0,
       "reversing_light": 0,
-      "indicator_left": 0,
+      "indicator_left": 255,
       "indicator_right": 0
     },
     "13": {
@@ -498,7 +497,7 @@ var hardware_test_configuration = {
       "brake_light": 0,
       "reversing_light": 0,
       "indicator_left": 0,
-      "indicator_right": 0
+      "indicator_right": 255
     },
     "14": {
       "max_change_per_systick": 0,
@@ -528,7 +527,7 @@ var hardware_test_configuration = {
       "light_switch_position7": 0,
       "light_switch_position8": 0,
       "tail_light": 0,
-      "brake_light": 0,
+      "brake_light": 255,
       "reversing_light": 0,
       "indicator_left": 0,
       "indicator_right": 0
@@ -555,7 +554,7 @@ var hardware_test_configuration = {
       "light_switch_position1": 0,
       "light_switch_position2": 0,
       "light_switch_position3": 0,
-      "light_switch_position4": 0,
+      "light_switch_position4": 255,
       "light_switch_position5": 0,
       "light_switch_position6": 0,
       "light_switch_position7": 0,
@@ -1100,7 +1099,7 @@ var hardware_test_configuration = {
     "led_count": 16
   },
   "gamma": {
-    "gamma_value": "2.2"
+    "gamma_value": "1.8"
   },
-  "light_programs": "// Test program\n//\n// This light controller firmware is designed to test the hardware.\n// As soon as power is applied it runs a test pattern on all LEDs.\n\nrun always\n\nled led0 = master[0]\nled led1 = master[1]\nled led2 = master[2]\nled led3 = master[3]\nled led4 = master[4]\nled led5 = master[5]\nled led6 = master[6]\nled led7 = master[7]\nled led8 = master[8]\nled led9 = master[9]\nled led10 = master[10]\nled led11 = master[11]\nled led12 = master[12]\nled led13 = master[13]\nled led14 = master[14]\nled led15 = master[15]\n\nvar brightness\nvar delay_time\n\nrestart:\n    fade all leds stepsize 0\n    all leds = 0%\n    brightness = 100    // percent\n    delay_time = 100    // milliseconds\n\n    // STEP 1: running lights, 100ms per LED, with decresing brightness in 5 steps\nchase:\n    led0 = brightness\n    sleep delay_time\n    led0 = 0\n    led1 = brightness\n    sleep delay_time\n    led1 = 0\n    led2 = brightness\n    sleep delay_time\n    led2 = 0\n    led3 = brightness\n    sleep delay_time\n    led3 = 0\n    led4 = brightness\n    sleep delay_time\n    led4 = 0\n    led5 = brightness\n    sleep delay_time\n    led5 = 0\n    led6 = brightness\n    sleep delay_time\n    led6 = 0\n    led7 = brightness\n    sleep delay_time\n    led7 = 0\n    led8 = brightness\n    sleep delay_time\n    led8 = 0\n    led9 = brightness\n    sleep delay_time\n    led9 = 0\n    led10 = brightness\n    sleep delay_time\n    led10 = 0\n    led11 = brightness\n    sleep delay_time\n    led11 = 0\n    led12 = brightness\n    sleep delay_time\n    led12 = 0\n    led13 = brightness\n    sleep delay_time\n    led13 = 0\n    led14 = brightness\n    sleep delay_time\n    led14 = 0\n    led15 = brightness\n    sleep delay_time\n    led15 = 0\n\n    brightness -= 20\n    skip if brightness < 20\n    goto chase\n\n\n    // STEP2: All leds on for 5 seconds\n    all leds = 100%\n    sleep 5000\n\n\n    // STEP3: All leds increasing brightness for 1 second\n    all leds = 20%\n    sleep 1000\n    all leds = 30%\n    sleep 1000\n    all leds = 60%\n    sleep 1000\n    all leds = 80%\n    sleep 1000\n    all leds = 100%\n    sleep 1000\n\n    goto restart\n\n    end\n"
+  "light_programs": "// Test program\n//\n// This light controller firmware is designed to test the hardware.\n// As soon as power is applied it runs a test pattern on all LEDs.\n\nrun always\n\nled led0 = led[0]\nled led1 = led[1]\nled led2 = led[2]\nled led3 = led[3]\nled led4 = led[4]\nled led5 = led[5]\nled led6 = led[6]\nled led7 = led[7]\nled led8 = led[8]\nled led9 = led[9]\nled led10 = led[10]\nled led11 = led[11]\nled led12 = led[12]\nled led13 = led[13]\nled led14 = led[14]\nled led15 = led[15]\n\nvar brightness\nvar delay_time\n\nrestart:\n    fade all leds stepsize 0\n    all leds = 0%\n    brightness = 100    // percent\n    delay_time = 100    // milliseconds\n\n    // STEP 1: running lights, 100ms per LED, with decresing brightness in 5 steps\nchase:\n    led0 = brightness\n    sleep delay_time\n    led0 = 0\n    led1 = brightness\n    sleep delay_time\n    led1 = 0\n    led2 = brightness\n    sleep delay_time\n    led2 = 0\n    led3 = brightness\n    sleep delay_time\n    led3 = 0\n    led4 = brightness\n    sleep delay_time\n    led4 = 0\n    led5 = brightness\n    sleep delay_time\n    led5 = 0\n    led6 = brightness\n    sleep delay_time\n    led6 = 0\n    led7 = brightness\n    sleep delay_time\n    led7 = 0\n    led8 = brightness\n    sleep delay_time\n    led8 = 0\n    led9 = brightness\n    sleep delay_time\n    led9 = 0\n    led10 = brightness\n    sleep delay_time\n    led10 = 0\n    led11 = brightness\n    sleep delay_time\n    led11 = 0\n    led12 = brightness\n    sleep delay_time\n    led12 = 0\n    led13 = brightness\n    sleep delay_time\n    led13 = 0\n    led14 = brightness\n    sleep delay_time\n    led14 = 0\n    led15 = brightness\n    sleep delay_time\n    led15 = 0\n\n    brightness -= 20\n    skip if brightness < 20\n    goto chase\n\n\n    // STEP2: All leds on for 5 seconds\n    all leds = 100%\n    sleep 5000\n\n\n    // STEP3: All leds increasing brightness for 1 second\n    all leds = 20%\n    sleep 1000\n    all leds = 30%\n    sleep 1000\n    all leds = 60%\n    sleep 1000\n    all leds = 80%\n    sleep 1000\n    all leds = 100%\n    sleep 1000\n\n    goto restart\n\n    end\n"
 }
