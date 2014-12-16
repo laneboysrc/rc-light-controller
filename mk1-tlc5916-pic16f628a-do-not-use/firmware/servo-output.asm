050a2d081ebb1be403d8bacf10f19ba365407560
LANE Boys RC <laneboysrc@gmail.com>
2014-12-16 17:04:54 +0800

    Move MK1 firmware to new folder

---
+++ mk1-tlc5916-pic16f628a-do-not-use/firmware/servo-output.asm
@@ -0,0 +1,98 @@
+;******************************************************************************
+;
+;   servo-output.asm
+;
+;******************************************************************************
+;
+;   Author:         Werner Lane
+;   E-mail:         laneboysrc@gmail.com
+;
+;******************************************************************************
+    TITLE       RC Light Controller - Servo output functions
+    LIST        r=dec
+    RADIX       dec
+
+    #include    hw.tmp
+
+    GLOBAL Init_servo_output
+    GLOBAL Make_servo_pulse 
+    
+    
+    ; Functions and variables imported from utils.asm
+    EXTERN Mul_x_by_6
+    EXTERN Add_x_and_780    
+
+    EXTERN xl
+    EXTERN xh
+    EXTERN yl
+    EXTERN yh
+    EXTERN zl
+    EXTERN zh
+
+
+IFNDEF ENABLE_SERVO_OUTPUT
+    ERROR "########################"
+    ERROR "To use the servo output you must add '-D ENABLE_SERVO_OUTPUT' to the CFLAGS in the makefile!"
+    ERROR "########################"
+ENDIF
+
+;******************************************************************************
+; Relocatable code section
+;*****************************************************************************
+.code_servo_output CODE
+
+;******************************************************************************
+; Servo output related initalization
+;******************************************************************************
+Init_servo_output
+    BANKSEL CCP1CON
+    movlw   b'00001010'
+            ; |||||||+ CCPM0 (Compare mode, generate software interrupt on 
+            ; ||||||+- CCPM1  match (CCP1IF bit is set, CCP1 pin is unaffected)
+            ; |||||+-- CCPM2 
+            ; ||||+--- CCPM3 
+            ; |||+---- CCP1Y (not used)
+            ; ||+----- CCP1X (not used)
+            ; |+------ 
+            ; +------- 
+    movwf   CCP1CON
+    return
+
+    
+;******************************************************************************
+; Make_servo_pulse 
+;
+; The percentage is given from -120 .. 0 .. 120% in the W register.
+;******************************************************************************
+Make_servo_pulse  
+    BANKSEL T1CON
+    addlw   120
+    movwf   xl
+    clrf    xh
+    call    Mul_x_by_6
+    call    Add_x_and_780
+
+    bcf     T1CON, TMR1ON   ; Stop timer 1
+    clrf    TMR1H           ; Reset the timer to 0
+    clrf    TMR1L
+    BANKSEL CCPR1L
+    movf    xl, w           ; Load Timer1 compare register with the servo time
+    movwf   CCPR1L
+    movf    xh, w
+    movwf   CCPR1H
+    BANKSEL PIR1
+    bcf     PIR1, CCP1IF    ; Clear Timer1 compare interrupt flag
+  
+    bsf     T1CON, TMR1ON   ; Start timer 1
+    bsf     PORT_SERVO      ; Set servo port to high pulse
+
+    btfss   PIR1, CCP1IF    ; Wait for compare value reached
+    goto    $ - 1
+
+    bcf     PORT_SERVO      ; Turn off servo pulse
+    bcf     T1CON, TMR1ON   ; Stop timer 1
+    bcf     PIR1, CCP1IF
+
+    return
+
+    END     ; Directive 'end of program'
