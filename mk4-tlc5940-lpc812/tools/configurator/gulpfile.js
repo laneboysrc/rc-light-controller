257f76798bf82c0815bdf3e9e2ed03c4e5c1a019
LANE Boys RC <laneboysrc@gmail.com>
2017-01-29 14:38:16 +0800

    Show warning error if users try to run the configurator source code
    If parser.js is not present then we show a large error message, instructing
    the user to download the correct file.
    It happened that someone downloaded the source code from Github and tried
    to load configurator.html, without having installed gulp or built the Light
    program assembler. Then simply nothing happens.
    
    To fix this we renamed configurator.html to source.html.
    
    We also upgraded gulp-inline-source and added error messages to the build
    process.

--- mk4-tlc5940-lpc812/tools/configurator/gulpfile.js
+++ mk4-tlc5940-lpc812/tools/configurator/gulpfile.js
@@ -4,6 +4,7 @@
 var gulp = require("gulp");
 var inlinesource = require('gulp-inline-source');
 var template = require('gulp-j140');
+var rename = require("gulp-rename");
 
 function get_date() {
     var date;
@@ -26,12 +27,13 @@ function get_date() {
 gulp.task('default', function () {
     var options = {
         compress: true,
-        swallowErrors: true,
+        swallowErrors: false,
     };
 
-    return gulp.src('./configurator.html')
+    return gulp.src('./source.html')
         .pipe(template({date: get_date()}))
-        .pipe(inlinesource(options))
+        .pipe(inlinesource(options)).on('error', errorHandler)
+        .pipe(rename('configurator.html'))
         .pipe(gulp.dest("./build"));
 });
 
@@ -39,10 +41,25 @@ gulp.task('default', function () {
 gulp.task('no-compress', function () {
     var options = {
         compress: false,
-        swallowErrors: true,
+        swallowErrors: false,
     };
 
-    return gulp.src('./configurator.html')
-        .pipe(inlinesource(options))
+    return gulp.src('./source.html')
+        .pipe(inlinesource(options)).on('error', errorHandler)
+        .pipe(rename('configurator.html'))
         .pipe(gulp.dest("./build"));
 });
+
+
+function errorHandler (error) {
+  console.log(error.toString());
+  console.log('');
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  console.log('In-lining of Javascript or CSS failed!');
+  console.log('');
+  console.log('Have you built the Light Programm assembler? Change to the "assembler/"');
+  console.log('directory and run "make".');
+  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  console.log('');
+  this.emit('exit');
+}
