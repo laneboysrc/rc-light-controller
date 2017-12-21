f77c1a453dfdd3a85c7040c8e0f270d3ee92812c
Werner Lane <laneboysrc@gmail.com>
2017-12-21 11:15:36 +0800

    Add bootloader, working!
    Missing:
    - Detect when app switched into bootloader
    - User can manually force the bootloader

---
+++ mk5-tlc5940-samd21/bootloader/main.c
@@ -0,0 +1,290 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <samd21.h>
+#include <usb.h>
+#include <flash_layout.h>
+#include <printf.h>
+
+
+#define UART_SERCOM SERCOM0
+#define UART_SERCOM_GCLK_ID SERCOM0_GCLK_ID_CORE
+#define UART_SERCOM_APBCMASK PM_APBCMASK_SERCOM0
+#define UART_TXD_PMUX PORT_PMUX_PMUXE_D_Val
+#define UART_RXD_PMUX PORT_PMUX_PMUXE_D_Val
+#define UART_TXD_PAD 0      // PAD0
+
+
+typedef struct {
+    uint8_t bit;
+    uint8_t port;
+    uint8_t mux;
+} gpio_t;
+
+
+// These are all defined by the linker via the linker script.
+extern uint32_t _ram;
+extern uint32_t _eram;
+
+static volatile uint32_t milliseconds;
+
+static const gpio_t GPIO_TXD = { .port = 0, .bit = 4, .mux = UART_TXD_PMUX };
+static const gpio_t GPIO_USB_DM = { .port = 0, .bit = 24, .mux = PORT_PMUX_PMUXE_G_Val };
+static const gpio_t GPIO_USB_DP = { .port = 0, .bit = 25, .mux = PORT_PMUX_PMUXE_G_Val };
+
+bool bootloader_done = false;
+
+// ****************************************************************************
+inline static void gpio_out(gpio_t gpio)
+{
+    PORT->Group[gpio.port].DIRSET.reg = (1<<gpio.bit);
+}
+
+
+// ****************************************************************************
+inline static void gpio_mux(gpio_t gpio)
+{
+    PORT->Group[gpio.port].PINCFG[gpio.bit].bit.PMUXEN = 1;
+    if (gpio.bit & 1) {
+        PORT->Group[gpio.port].PMUX[gpio.bit >> 1].bit.PMUXO = gpio.mux;
+    }
+    else {
+        PORT->Group[gpio.port].PMUX[gpio.bit >> 1].bit.PMUXE = gpio.mux;
+    }
+}
+
+
+// ****************************************************************************
+static void init_uart(uint32_t baudrate)
+{
+    #define UART_CLK 48000000
+
+    uint64_t brr = (uint64_t)65536 * (UART_CLK - 16 * baudrate) / UART_CLK;
+
+    gpio_out(GPIO_TXD);
+    gpio_mux(GPIO_TXD);
+
+    // Turn on power to the USART peripheral
+    PM->APBCMASK.reg |= UART_SERCOM_APBCMASK;
+
+    // Use GLKGEN0 (8 MHz) as clock source for the UART
+    GCLK->CLKCTRL.reg =
+        GCLK_CLKCTRL_ID(UART_SERCOM_GCLK_ID) |
+        GCLK_CLKCTRL_CLKEN |
+        GCLK_CLKCTRL_GEN(0);
+
+    // Run UART from GCLK; Setup Tx pad
+    UART_SERCOM->USART.CTRLA.reg =
+        SERCOM_USART_CTRLA_DORD |
+        SERCOM_USART_CTRLA_MODE_USART_INT_CLK |
+        SERCOM_USART_CTRLA_TXPO(UART_TXD_PAD);
+
+    // Enable transmit only; 8 bit characters
+    UART_SERCOM->USART.CTRLB.reg =
+        SERCOM_USART_CTRLB_TXEN |
+        SERCOM_USART_CTRLB_CHSIZE(0);           // 8 bits
+    while (UART_SERCOM->USART.SYNCBUSY.reg);
+
+    UART_SERCOM->USART.BAUD.reg = (uint16_t)brr;
+    while (UART_SERCOM->USART.SYNCBUSY.reg);
+
+    UART_SERCOM->USART.CTRLA.reg |= SERCOM_USART_CTRLA_ENABLE;
+    while (UART_SERCOM->USART.SYNCBUSY.reg);
+}
+
+
+// ****************************************************************************
+static void uart_putc(void *p, char c)
+{
+    (void) p;
+    while (!(UART_SERCOM->USART.INTFLAG.reg & SERCOM_USART_INTFLAG_DRE));
+    UART_SERCOM->USART.DATA.reg = c;
+}
+
+
+// ****************************************************************************
+static void init_systick(void)
+{
+    // Configure the SYSTICK to create an interrupt every 1 millisecond
+    SysTick_Config(48000);
+    NVIC_SetPriority(SysTick_IRQn, 0x0);
+
+    __enable_irq();
+}
+
+
+// ****************************************************************************
+void SysTick_Handler(void)
+{
+    ++milliseconds;
+}
+
+// ****************************************************************************
+static void delay_ms(uint32_t value_ms) {
+    uint32_t end_ms = milliseconds + value_ms;
+
+    while (milliseconds < end_ms) {
+        __WFI();
+    }
+}
+
+// ****************************************************************************
+static void init_clock(void)
+{
+    uint32_t *coarse_p;
+    uint32_t coarse;
+
+    // ------------------------------------------------
+    // Since we intend to run at 48 MHz system clock, we neeed to conigure
+    // one wait state for the flash according to the datasheet.
+    NVMCTRL->CTRLB.reg = NVMCTRL_CTRLB_RWS(1);
+
+    // ------------------------------------------------
+    // Set up the PLL to provide a 48 MHz system clock
+    SYSCTRL->INTFLAG.reg =
+            SYSCTRL_INTFLAG_BOD33RDY |
+            SYSCTRL_INTFLAG_BOD33DET |
+            SYSCTRL_INTFLAG_DFLLRDY;
+
+    SYSCTRL->DFLLCTRL.reg = 0;
+    while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+    SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_MUL(48000);
+
+    // Load PLL calibration values from NVRAM
+    coarse_p = (uint32_t *)SYSCTRL_FUSES_DFLL48M_COARSE_CAL_ADDR;
+    coarse = (*coarse_p & SYSCTRL_FUSES_DFLL48M_COARSE_CAL_Msk) >> SYSCTRL_FUSES_DFLL48M_COARSE_CAL_Pos;
+    SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(512);
+
+    SYSCTRL->DFLLCTRL.reg =
+            SYSCTRL_DFLLCTRL_ENABLE |
+            SYSCTRL_DFLLCTRL_USBCRM |
+            SYSCTRL_DFLLCTRL_BPLCKC |
+            SYSCTRL_DFLLCTRL_STABLE |
+            SYSCTRL_DFLLCTRL_CCDIS;
+
+    while (!(SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+
+    // ------------------------------------------------
+    // Reset the generic clock control block
+    GCLK->CTRL.reg = GCLK_CTRL_SWRST;
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+
+    // Setup GENCLK0 to run at 48 MHz. This clock is used for high speed
+    // peripherals such as USB and UART
+    GCLK->GENDIV.reg =
+        GCLK_GENDIV_ID(0) |
+        GCLK_GENDIV_DIV(1);
+
+    GCLK->GENCTRL.reg =
+            GCLK_GENCTRL_ID(0) |
+            GCLK_GENCTRL_SRC_DFLL48M |
+            GCLK_GENCTRL_RUNSTDBY |
+            GCLK_GENCTRL_GENEN;
+
+    while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+}
+
+
+// ****************************************************************************
+static void init_usb(void)
+{
+    gpio_mux(GPIO_USB_DM);
+    gpio_mux(GPIO_USB_DP);
+
+    usb_init();
+    usb_attach();
+}
+
+// ****************************************************************************
+static void run_application(void)
+{
+    uint32_t msp;
+    uint32_t reset_vector;
+
+    msp = ((uint32_t *)FLASH_FIRMWARE_START)[0];
+    reset_vector = ((uint32_t *)FLASH_FIRMWARE_START)[1];
+
+    // Disable all interrupts and the systick timer
+    __disable_irq();
+    SysTick->CTRL = 0;
+
+    // Switch to the the interrupt vector table of the application
+    SCB->VTOR = FLASH_FIRMWARE_START & SCB_VTOR_TBLOFF_Msk;
+
+    // Set up the stack pointer
+    __set_MSP(msp);
+
+    // Jump to the application
+    __asm__ volatile("bx %0" :: "r" (reset_vector));
+}
+
+
+// ****************************************************************************
+static bool application_is_present(void)
+{
+    uint32_t sp;
+    uint32_t pc;
+    uint32_t ram;
+    uint32_t ram_end;
+
+    sp = ((uint32_t *)FLASH_FIRMWARE_START)[0];
+    pc = ((uint32_t *)FLASH_FIRMWARE_START)[1];
+    ram = HMCRAMC0_ADDR;
+    ram_end = HMCRAMC0_ADDR + HMCRAMC0_SIZE;
+
+    return ((sp >= ram)  &&  (sp <= ram_end)  &&  (pc < FLASH_SIZE));
+}
+
+// ****************************************************************************
+static void print_app_diagnostics(void)
+{
+    uint32_t sp;
+    uint32_t pc;
+
+    sp = ((uint32_t *)FLASH_FIRMWARE_START)[0];
+    pc = ((uint32_t *)FLASH_FIRMWARE_START)[1];
+
+    printf("MSP             0x%08x\n", sp);
+    printf("PC              0x%08x\n", pc);
+}
+
+
+// ****************************************************************************
+static void bootloader(void)
+{
+    init_clock();
+    init_systick();
+    init_uart(115200);
+    init_printf(0, uart_putc);
+    init_usb();
+
+    printf("\n\n**********\nRC Light Controller Bootloader\n");
+
+    print_app_diagnostics();
+
+    while (!bootloader_done) {
+        if ((milliseconds % 1000) == 0) {
+            printf("%u\n", milliseconds / 1000);
+        }
+        __WFI();
+    }
+
+    printf("DFU completed, rebooting!\n");
+    delay_ms(25);
+    usb_detach();
+    delay_ms(100);
+    NVIC_SystemReset();
+}
+
+
+// ****************************************************************************
+int main(void)
+{
+    if (!application_is_present()) {
+        bootloader();
+    }
+
+    run_application();
+}
---
+++ mk5-tlc5940-samd21/bootloader/usb.c
@@ -0,0 +1,437 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdalign.h>
+
+#include <usb.h>
+#include <usb_samd.h>
+#include <class/dfu/dfu.h>
+#include <flash_layout.h>
+#include <printf.h>
+
+
+// We can only erase the flash in units of 'row', where each row consists of
+// four pages. We therefore have the DFU transfer in blocks of 4 pages = 1 ROW
+#define DFU_TRANSFER_SIZE (FLASH_PAGE_SIZE * 4)
+
+#define DFU_RUNTIME_PROTOCOL 1
+#define DFU_DFU_MODE_PROTOCOL 2
+
+#define USB_INTERFACE_DFU 0
+
+#define USB_STRING_LANGUAGE 0
+#define USB_STRING_MANUFACTURER 1
+#define USB_STRING_PRODUCT 2
+#define USB_STRING_SERIAL_NUMBER 3
+#define USB_STRING_DFU 4
+#define USB_STRING_MSFT 0xee
+
+#define USB_EP0 (USB_IN + 0)
+
+#define MSFT_ID 0xee
+
+
+USB_ENDPOINTS(1)
+
+alignas(4) uint8_t ep0_buffer[146];
+
+extern bool bootloader_done;
+
+
+
+__attribute__((__aligned__(4))) const USB_DeviceDescriptor device_descriptor = {
+    .bLength = sizeof(USB_DeviceDescriptor),
+    .bDescriptorType = USB_DTYPE_Device,
+
+    .bcdUSB                 = 0x0200,
+    .bDeviceClass           = USB_CSCP_NoDeviceClass,
+    .bDeviceSubClass        = USB_CSCP_NoDeviceSubclass,
+    .bDeviceProtocol        = USB_CSCP_NoDeviceProtocol,
+
+    .bMaxPacketSize0        = 64,
+    .idVendor               = 0x6666,
+    .idProduct              = 0xcab0,
+    .bcdDevice              = 0x0111,
+
+    .iManufacturer          = USB_STRING_MANUFACTURER,
+    .iProduct               = USB_STRING_PRODUCT,
+    .iSerialNumber          = USB_STRING_SERIAL_NUMBER,
+
+    .bNumConfigurations     = 1
+};
+
+
+typedef struct ConfigDesc {
+    USB_ConfigurationDescriptor Config;
+
+    USB_InterfaceDescriptor DFU_interface;
+    DFU_FunctionalDescriptor DFU_functional;
+
+}  __attribute__((packed)) ConfigDesc;
+
+alignas(4) const ConfigDesc configuration_descriptor = {
+    .Config = {
+        .bLength = sizeof(USB_ConfigurationDescriptor),
+        .bDescriptorType = USB_DTYPE_Configuration,
+        .wTotalLength  = sizeof(ConfigDesc),
+        .bNumInterfaces = 1,
+        .bConfigurationValue = 1,
+        .iConfiguration = 0,
+        .bmAttributes = USB_CONFIG_ATTR_BUSPOWERED,
+        .bMaxPower = USB_CONFIG_POWER_MA(100)
+    },
+
+    .DFU_interface = {
+        .bLength = sizeof(USB_InterfaceDescriptor),
+        .bDescriptorType = USB_DTYPE_Interface,
+        .bInterfaceNumber = USB_INTERFACE_DFU,
+        .bAlternateSetting = 0,
+        .bNumEndpoints = 0,
+        .bInterfaceClass = DFU_INTERFACE_CLASS,
+        .bInterfaceSubClass = DFU_INTERFACE_SUBCLASS,
+        .bInterfaceProtocol = DFU_DFU_MODE_PROTOCOL,
+        .iInterface = USB_STRING_DFU
+    },
+    .DFU_functional = {
+        .bLength = sizeof(DFU_FunctionalDescriptor),
+        .bDescriptorType = DFU_DESCRIPTOR_TYPE,
+        .bmAttributes = DFU_ATTR_CAN_DOWNLOAD | DFU_ATTR_WILL_DETACH,
+        .wDetachTimeout = 0,
+        .wTransferSize = DFU_TRANSFER_SIZE,
+        .bcdDFUVersion = 0x0110,
+    }
+};
+
+
+typedef struct {
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    __CHAR16_TYPE__ bString[1];
+} __attribute__ ((packed)) language_string_t;
+
+alignas(4) const language_string_t language_string = {
+    .bLength = USB_STRING_LEN(1),
+    .bDescriptorType = USB_DTYPE_String,
+    .bString = { USB_LANGUAGE_EN_US }
+};
+
+
+typedef struct {
+    uint8_t bLength;
+    uint8_t bDescriptorType;
+    __CHAR16_TYPE__ bString[7];
+    uint8_t bVendorCode;
+    uint8_t bPadding;
+} __attribute__((packed)) msft_os_t;
+
+alignas(4) const msft_os_t msft_os = {
+    .bLength = 18,
+    .bDescriptorType = USB_DTYPE_String,
+    .bString = { u"MSFT100" },
+    .bVendorCode = 0x42,
+    .bPadding = 0
+};
+
+
+typedef struct {
+    uint32_t dwLength;
+    uint16_t bcdVersion;
+    uint16_t wIndex;
+    uint8_t bCount;
+    uint8_t reserved[7];
+    USB_MicrosoftCompatibleDescriptor_Interface interfaces[2];
+} __attribute__((packed)) msft_compatible_t;
+
+
+const msft_compatible_t msft_compatible = {
+    .dwLength = sizeof(USB_MicrosoftCompatibleDescriptor) + (2 * sizeof(USB_MicrosoftCompatibleDescriptor_Interface)),
+    .bcdVersion = 0x0100,
+    .wIndex = 0x0004,
+    .bCount = 1,
+    .reserved = {0, 0, 0, 0, 0, 0, 0},
+    .interfaces = {
+        {
+            .bFirstInterfaceNumber = 0,
+            .reserved1 = 0x01,
+            .compatibleID = "WINUSB\0\0",
+            .subCompatibleID = {0, 0, 0, 0, 0, 0, 0, 0},
+            .reserved2 = {0, 0, 0, 0, 0, 0},
+        }
+    }
+};
+
+typedef struct {
+    uint32_t dwLength;
+    uint16_t bcdVersion;
+    uint16_t wIndex;
+    uint16_t wCount;
+    uint32_t dwPropLength;
+    uint32_t dwType;
+    uint16_t wNameLength;
+    uint16_t name[21];
+    uint32_t dwDataLength;
+    uint16_t data[40];
+    uint8_t _padding[2];
+} __attribute__((packed)) USB_MicrosoftExtendedPropertiesDescriptor;
+
+// FIXME: check this...
+const USB_MicrosoftExtendedPropertiesDescriptor msft_extended = {
+    .dwLength = 146,
+    .bcdVersion = 0x0100,
+    .wIndex = 0x05,
+    .wCount = 0x01,
+    .dwPropLength = 136,
+    .dwType = 7,
+    .wNameLength = 42,
+    .name = u"DeviceInterfaceGUIDs\0",
+    .dwDataLength = 78,
+    .data = u"{3c33bbfd-71f9-4815-8b8f-7cd1ef928b3d}",
+};
+
+
+void dfu_cb_dnload_block(uint16_t block_number, uint16_t length) {
+    uint32_t address;
+
+    (void) length;
+
+    if (usb_setup.wLength > DFU_TRANSFER_SIZE) {
+        dfu_error(DFU_STATUS_errUNKNOWN);
+        return;
+    }
+
+    if ((block_number * DFU_TRANSFER_SIZE) > FLASH_FIRMWARE_SIZE) {
+        dfu_error(DFU_STATUS_errADDRESS);
+        return;
+    }
+
+    printf("dfu_cb_dnload_block %d len=%d\n", block_number, length);
+
+    // Erase the of flash memory row that corresponds to the given block_number
+    address = FLASH_FIRMWARE_START + (block_number * DFU_TRANSFER_SIZE);
+    NVMCTRL->ADDR.reg = address >> 1;
+    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD(NVMCTRL_CTRLA_CMD_ER);
+    while (!NVMCTRL->INTFLAG.bit.READY);
+
+    printf("done\n");
+}
+
+void dfu_cb_dnload_packet_completed(uint16_t block_number, uint16_t offset, uint8_t* data, uint16_t length) {
+    uint32_t address;
+    uint32_t nvm_address;
+
+    address = FLASH_FIRMWARE_START + (block_number * DFU_TRANSFER_SIZE) + offset;
+    nvm_address = address / 2;
+
+    // The NVM must be accessed as a series of 16-bit words. The tricky bit
+    // is that we must ensure that we can write an odd number of bytes, i.e.
+    // the last word may be incomplete.
+    for (uint16_t i = 0; i < length; i += 2) {
+        uint16_t word;
+
+        word = data[i];
+        if ((i + 1) < length) {
+            word |= data[i + 1] << 8;
+        }
+
+        ((volatile uint16_t *)FLASH_ADDR)[nvm_address++] = word;
+    }
+
+    // Perform the write
+    NVMCTRL->CTRLA.reg = NVMCTRL_CTRLA_CMDEX_KEY | NVMCTRL_CTRLA_CMD(NVMCTRL_CTRLA_CMD_WP);
+    while (!NVMCTRL->INTFLAG.bit.READY);
+
+    printf("dfu_cb_dnload_packet_completed %d len=%d\n", block_number, length);
+}
+
+unsigned dfu_cb_dnload_block_completed(uint16_t block_number, uint16_t length) {
+    (void) block_number;
+    (void) length;
+
+    printf("dfu_cb_dnload_block_completed %d len=%d\n", block_number, length);
+    return 0;
+}
+
+
+void dfu_cb_manifest(void) {
+    printf("dfu_cb_manifest\n");
+    bootloader_done = true;
+}
+
+
+
+
+
+uint16_t usb_cb_get_descriptor(uint8_t type, uint8_t index, const uint8_t** ptr) {
+    const void* address = NULL;
+    uint16_t size = 0;
+
+    // printf("usb_cb_get_descriptor\n");
+
+    switch (type) {
+        case USB_DTYPE_Device:
+            address = &device_descriptor;
+            size    = sizeof(USB_DeviceDescriptor);
+            break;
+
+        case USB_DTYPE_Configuration:
+            address = &configuration_descriptor;
+            size = sizeof(ConfigDesc);
+            break;
+
+        case USB_DTYPE_String:
+            switch (index) {
+                case USB_STRING_LANGUAGE:
+                    // address = &language_string;
+                    address = &language_string;
+                    break;
+
+                case USB_STRING_MANUFACTURER:
+                    address = usb_string_to_descriptor((char *)"LANE Boys RC");
+                    break;
+
+                case USB_STRING_PRODUCT:
+                    address = usb_string_to_descriptor((char *)"RC Light Controller");
+                    break;
+
+                case USB_STRING_SERIAL_NUMBER:
+                    address = samd_serial_number_string_descriptor();
+                    break;
+
+                case USB_STRING_DFU:
+                    address = usb_string_to_descriptor((char *)"Firmware update");
+                    break;
+
+                case USB_STRING_MSFT:
+                    address = &msft_os;
+                    break;
+
+                default:
+                    break;
+            }
+            size = (((USB_StringDescriptor*)address))->bLength;
+            break;
+
+        default:
+            break;
+    }
+
+    *ptr = address;
+    return size;
+}
+
+void usb_cb_reset(void) {
+    printf("usb_cb_reset\n");
+}
+
+bool usb_cb_set_configuration(uint8_t config) {
+    printf("usb_cb_set_configuration\n");
+    if (config <= 1) {
+        return true;
+    }
+    return false;
+}
+
+
+static void handle_msft_compatible(
+    const USB_MicrosoftCompatibleDescriptor* msft_descriptor,
+    const USB_MicrosoftExtendedPropertiesDescriptor* msft_extended_descriptor)
+{
+    uint16_t len;
+    if (usb_setup.wIndex == 0x0005) {
+        len = msft_extended_descriptor->dwLength;
+        memcpy(ep0_buffer, msft_extended_descriptor, len);
+    }
+    else if (usb_setup.wIndex == 0x0004) {
+        len = msft_descriptor->dwLength;
+        memcpy(ep0_buffer, msft_descriptor, len);
+    }
+    else {
+        usb_ep0_stall();
+        return;
+    }
+
+    if (len > usb_setup.wLength) {
+        len = usb_setup.wLength;
+    }
+
+    usb_ep_start_in(USB_EP0, ep0_buffer, len, true);
+    usb_ep0_out();
+}
+
+
+void usb_cb_control_setup(void) {
+    uint8_t recipient = usb_setup.bmRequestType & USB_REQTYPE_RECIPIENT_MASK;
+
+    printf("usb_cb_control_setup\n");
+    if (recipient == USB_RECIPIENT_INTERFACE) {
+        // Forward all DFU related requests
+        if (usb_setup.wIndex == USB_INTERFACE_DFU) {
+            dfu_control_setup();
+            return;
+        }
+
+        switch(usb_setup.bRequest) {
+            case MSFT_ID:
+                // FIXME: only one should be here, read docs!
+                handle_msft_compatible((USB_MicrosoftCompatibleDescriptor *)&msft_compatible, &msft_extended);
+                return;
+
+            default:
+                break;
+        }
+    }
+
+    else if (recipient == USB_RECIPIENT_DEVICE) {
+        switch(usb_setup.bRequest) {
+            case MSFT_ID:
+                // FIXME: only one should be here, read docs!
+                handle_msft_compatible((USB_MicrosoftCompatibleDescriptor *)&msft_compatible, &msft_extended);
+                return;
+
+            default:
+                break;
+        }
+    }
+
+    usb_ep0_stall();
+    return;
+}
+
+void usb_cb_control_in_completion(void) {
+    uint8_t recipient = usb_setup.bmRequestType & USB_REQTYPE_RECIPIENT_MASK;
+
+    if (recipient == USB_RECIPIENT_INTERFACE) {
+        if (usb_setup.wIndex ==USB_INTERFACE_DFU) {
+            dfu_control_in_completion();
+        }
+    }
+}
+
+void usb_cb_control_out_completion(void) {
+    uint8_t recipient = usb_setup.bmRequestType & USB_REQTYPE_RECIPIENT_MASK;
+
+    if (recipient == USB_RECIPIENT_INTERFACE) {
+        if (usb_setup.wIndex == USB_INTERFACE_DFU) {
+            dfu_control_out_completion();
+        }
+    }
+}
+
+
+void usb_cb_completion(void) {
+    // printf("usb_cb_completion\n");
+}
+
+
+bool usb_cb_set_interface(uint16_t interface, uint16_t new_altsetting) {
+    (void) new_altsetting;
+
+    printf("usb_cb_set_interface %d\n", interface);
+
+    if (interface == USB_INTERFACE_DFU) {
+        NVMCTRL->CTRLB.bit.MANW = 1;
+        dfu_reset();
+        return true;
+    }
+
+    return false;
+}
