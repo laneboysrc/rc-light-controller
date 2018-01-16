08133a1def5e8fd67e24ede72d85308950b84485
LANE Boys RC <laneboysrc@gmail.com>
2018-01-16 20:28:03 +0800

    Fix issue with Spektrum and Quicrun 1080 ESC
    The issue is that the serial output caused generated by the
    Quicrun ESC is interfering with the servo reader initialization.
    Simply initializing the servo reader only after 100 ms resolves
    the issue.

--- mk4-tlc5940-lpc812/firmware/main.c
+++ mk4-tlc5940-lpc812/firmware/main.c
@@ -127,15 +127,20 @@ int main(void)
     load_persistent_storage();
     HAL_uart_init(config.baudrate);
     init_printf(STDOUT, HAL_putc);
-    init_servo_reader();
-    init_uart_reader();
-    init_servo_output();
 
     // Wait for 100ms to have the supply settle down before initializing the
     // rest of the system. This is especially important for the TLC5940,
     // which misbehaves (certain LEDs don't work) when being addressed before
     // power is stable.
+    //
+    // It is also important in terms of servo reader function, because some
+    // RC electronics like the Hobbywing Quicrun 1080 are outputing serial
+    // data after startup, which interferes with the initialization procedure
     while (milliseconds <  100);
+
+    init_servo_reader();
+    init_uart_reader();
+    init_servo_output();
 
     init_lights();
     HAL_hardware_init_final();
