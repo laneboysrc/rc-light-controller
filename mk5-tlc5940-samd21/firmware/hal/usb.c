32bc77647a6d5d39998da6e73d0be351437fed19
LANE Boys RC <laneboysrc@gmail.com>
2018-03-10 09:07:44 +0800

    Increment version number
    For Windows, every time the descriptors change the device
    version must be incremented as Windows 8.1 otherwise messes
    up and claims no driver is installed.

--- mk5-tlc5940-samd21/firmware/hal/usb.c
+++ mk5-tlc5940-samd21/firmware/hal/usb.c
@@ -4,7 +4,7 @@
 #include <hal.h>
 #include <usb.h>
 #include <usb_samd.h>
-#include <class/cdc/cdc_standard.h>
+// #include <class/cdc/cdc_standard.h>
 #include <class/dfu/dfu.h>
 #include <usb_bos.h>
 
@@ -14,17 +14,9 @@ extern void add_uint8_to_receive_buffer(uint8_t byte);
 
 // Transfer size conforms to one flash 'row', which consists of four 'pages'
 #define DFU_TRANSFER_SIZE (FLASH_PAGE_SIZE * 4)
+
 #define DFU_RUNTIME_PROTOCOL 1
 #define DFU_DFU_PROTOCOL 2
-
-USB_ENDPOINTS(3)
-
-#define USB_STRING_LANGUAGE 0
-#define USB_STRING_MANUFACTURER 1
-#define USB_STRING_PRODUCT 2
-#define USB_STRING_SERIAL_NUMBER 3
-#define USB_STRING_DFU 4
-#define USB_STRING_UART 5
 
 #define USB_DTYPE_BOS 15
 #define WEBUSB_REQUEST_GET_URL 2
@@ -32,22 +24,33 @@ USB_ENDPOINTS(3)
 
 enum {
     USB_INTERFACE_DFU,
-    USB_INTERFACE_CDC_CONTROL,
-    USB_INTERFACE_CDC_DATA,
+    // USB_INTERFACE_CDC_CONTROL,
+    // USB_INTERFACE_CDC_DATA,
     USB_NUMBER_OF_INTERFACES
 };
 
+#define USB_STRING_LANGUAGE 0
+#define USB_STRING_MANUFACTURER 1
+#define USB_STRING_PRODUCT 2
+#define USB_STRING_SERIAL_NUMBER 3
+#define USB_STRING_DFU 4
+// #define USB_STRING_UART 5
+
+
 #define USB_EP0 (USB_IN + 0)
-#define USB_EP_CDC_NOTIFICATION (USB_IN + 1)
-#define USB_EP_CDC_IN (USB_IN + 2)
-#define USB_EP_CDC_OUT (USB_OUT + 3)
-
-#define BUF_SIZE 64
+// #define USB_EP_CDC_NOTIFICATION (USB_IN + 1)
+// #define USB_EP_CDC_IN (USB_IN + 2)
+// #define USB_EP_CDC_OUT (USB_OUT + 3)
 
 
-static alignas(4) uint8_t usbserial_buf_in[BUF_SIZE];
-static alignas(4) uint8_t usbserial_buf_out[BUF_SIZE];
-static bool usbserial_buf_in_busy = false;
+// Declare the endpoints in use.
+USB_ENDPOINTS(1)
+
+// #define BUF_SIZE 64
+
+// static alignas(4) uint8_t usbserial_buf_in[BUF_SIZE];
+// static alignas(4) uint8_t usbserial_buf_out[BUF_SIZE];
+// static bool usbserial_buf_in_busy = false;
 
 static const uint8_t* data;
 static uint16_t data_length;
@@ -61,17 +64,17 @@ typedef struct {
     USB_InterfaceDescriptor DFU_interface;
     DFU_FunctionalDescriptor DFU_functional;
 
-    USB_InterfaceAssociationDescriptor CDC_interface_association;
+    // USB_InterfaceAssociationDescriptor CDC_interface_association;
 
-    USB_InterfaceDescriptor CDC_control_interface;
-    CDC_FunctionalHeaderDescriptor CDC_functional_header;
-    CDC_FunctionalACMDescriptor CDC_functional_ACM;
-    CDC_FunctionalUnionDescriptor CDC_functional_union;
-    USB_EndpointDescriptor CDC_notification_endpoint;
+    // USB_InterfaceDescriptor CDC_control_interface;
+    // CDC_FunctionalHeaderDescriptor CDC_functional_header;
+    // CDC_FunctionalACMDescriptor CDC_functional_ACM;
+    // CDC_FunctionalUnionDescriptor CDC_functional_union;
+    // USB_EndpointDescriptor CDC_notification_endpoint;
 
-    USB_InterfaceDescriptor CDC_data_interface;
-    USB_EndpointDescriptor CDC_out_endpoint;
-    USB_EndpointDescriptor CDC_in_endpoint;
+    // USB_InterfaceDescriptor CDC_data_interface;
+    // USB_EndpointDescriptor CDC_out_endpoint;
+    // USB_EndpointDescriptor CDC_in_endpoint;
 }  __attribute__((packed)) configuration_descriptor_t;
 
 typedef struct {
@@ -93,7 +96,7 @@ static const USB_DeviceDescriptor device_descriptor = {
     .bMaxPacketSize0 = 64,
     .idVendor = 0x6666,
     .idProduct = 0xcab1,
-    .bcdDevice = 0x0103,
+    .bcdDevice = 0x0104,
 
     .iManufacturer = USB_STRING_MANUFACTURER,
     .iProduct = USB_STRING_PRODUCT,
@@ -106,7 +109,7 @@ static const configuration_descriptor_t configuration_descriptor = {
     .Config = {
         .bLength = sizeof(USB_ConfigurationDescriptor),
         .bDescriptorType = USB_DTYPE_Configuration,
-        .wTotalLength  = sizeof(configuration_descriptor),
+        .wTotalLength  = sizeof(configuration_descriptor_t),
         .bNumInterfaces = USB_NUMBER_OF_INTERFACES,
         .bConfigurationValue = 1,
         .iConfiguration = 0,
@@ -145,83 +148,83 @@ static const configuration_descriptor_t configuration_descriptor = {
         .bcdDFUVersion = 0x0110,
     },
 
-    .CDC_interface_association = {
-        .bLength = sizeof(USB_InterfaceAssociationDescriptor),
-        .bDescriptorType = USB_DTYPE_InterfaceAssociation,
-        .bFirstInterface = USB_INTERFACE_CDC_CONTROL,
-        .bInterfaceCount = 2,
-        .bFunctionClass = CDC_INTERFACE_CLASS,
-        .bFunctionSubClass = CDC_INTERFACE_SUBCLASS_ACM,
-        .bFunctionProtocol = 0,         // USB_CDC_PROTOCOL_NONE
-        .iFunction = USB_STRING_UART,
-    },
+    // .CDC_interface_association = {
+    //     .bLength = sizeof(USB_InterfaceAssociationDescriptor),
+    //     .bDescriptorType = USB_DTYPE_InterfaceAssociation,
+    //     .bFirstInterface = USB_INTERFACE_CDC_CONTROL,
+    //     .bInterfaceCount = 2,
+    //     .bFunctionClass = CDC_INTERFACE_CLASS,
+    //     .bFunctionSubClass = CDC_INTERFACE_SUBCLASS_ACM,
+    //     .bFunctionProtocol = 0,         // USB_CDC_PROTOCOL_NONE
+    //     .iFunction = USB_STRING_UART,
+    // },
 
-    .CDC_control_interface = {
-        .bLength = sizeof(USB_InterfaceDescriptor),
-        .bDescriptorType = USB_DTYPE_Interface,
-        .bInterfaceNumber = USB_INTERFACE_CDC_CONTROL,
-        .bAlternateSetting = 0,
-        .bNumEndpoints = 1,
-        .bInterfaceClass = CDC_INTERFACE_CLASS,
-        .bInterfaceSubClass = CDC_INTERFACE_SUBCLASS_ACM,
-        .bInterfaceProtocol = 0,
-        .iInterface = USB_STRING_UART,
-    },
-    .CDC_functional_header = {
-        .bLength = sizeof(CDC_FunctionalHeaderDescriptor),
-        .bDescriptorType = USB_DTYPE_CSInterface,
-        .bDescriptorSubtype = CDC_SUBTYPE_HEADER,
-        .bcdCDC = 0x0110,
-    },
-    .CDC_functional_ACM = {
-        .bLength = sizeof(CDC_FunctionalACMDescriptor),
-        .bDescriptorType = USB_DTYPE_CSInterface,
-        .bDescriptorSubtype = CDC_SUBTYPE_ACM,
-        .bmCapabilities = 0x00,
-    },
-    .CDC_functional_union = {
-        .bLength = sizeof(CDC_FunctionalUnionDescriptor),
-        .bDescriptorType = USB_DTYPE_CSInterface,
-        .bDescriptorSubtype = CDC_SUBTYPE_UNION,
-        .bMasterInterface = USB_INTERFACE_CDC_CONTROL,
-        .bSlaveInterface = USB_INTERFACE_CDC_DATA,
-    },
-    .CDC_notification_endpoint = {
-        .bLength = sizeof(USB_EndpointDescriptor),
-        .bDescriptorType = USB_DTYPE_Endpoint,
-        .bEndpointAddress = USB_EP_CDC_NOTIFICATION,
-        .bmAttributes = (USB_EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
-        .wMaxPacketSize = 8,
-        .bInterval = 0xFF
-    },
+    // .CDC_control_interface = {
+    //     .bLength = sizeof(USB_InterfaceDescriptor),
+    //     .bDescriptorType = USB_DTYPE_Interface,
+    //     .bInterfaceNumber = USB_INTERFACE_CDC_CONTROL,
+    //     .bAlternateSetting = 0,
+    //     .bNumEndpoints = 1,
+    //     .bInterfaceClass = CDC_INTERFACE_CLASS,
+    //     .bInterfaceSubClass = CDC_INTERFACE_SUBCLASS_ACM,
+    //     .bInterfaceProtocol = 0,
+    //     .iInterface = USB_STRING_UART,
+    // },
+    // .CDC_functional_header = {
+    //     .bLength = sizeof(CDC_FunctionalHeaderDescriptor),
+    //     .bDescriptorType = USB_DTYPE_CSInterface,
+    //     .bDescriptorSubtype = CDC_SUBTYPE_HEADER,
+    //     .bcdCDC = 0x0110,
+    // },
+    // .CDC_functional_ACM = {
+    //     .bLength = sizeof(CDC_FunctionalACMDescriptor),
+    //     .bDescriptorType = USB_DTYPE_CSInterface,
+    //     .bDescriptorSubtype = CDC_SUBTYPE_ACM,
+    //     .bmCapabilities = 0x00,
+    // },
+    // .CDC_functional_union = {
+    //     .bLength = sizeof(CDC_FunctionalUnionDescriptor),
+    //     .bDescriptorType = USB_DTYPE_CSInterface,
+    //     .bDescriptorSubtype = CDC_SUBTYPE_UNION,
+    //     .bMasterInterface = USB_INTERFACE_CDC_CONTROL,
+    //     .bSlaveInterface = USB_INTERFACE_CDC_DATA,
+    // },
+    // .CDC_notification_endpoint = {
+    //     .bLength = sizeof(USB_EndpointDescriptor),
+    //     .bDescriptorType = USB_DTYPE_Endpoint,
+    //     .bEndpointAddress = USB_EP_CDC_NOTIFICATION,
+    //     .bmAttributes = (USB_EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+    //     .wMaxPacketSize = 8,
+    //     .bInterval = 0xFF
+    // },
 
-    .CDC_data_interface = {
-        .bLength = sizeof(USB_InterfaceDescriptor),
-        .bDescriptorType = USB_DTYPE_Interface,
-        .bInterfaceNumber = USB_INTERFACE_CDC_DATA,
-        .bAlternateSetting = 0,
-        .bNumEndpoints = 2,
-        .bInterfaceClass = CDC_INTERFACE_CLASS_DATA,
-        .bInterfaceSubClass = 0,
-        .bInterfaceProtocol = 0,
-        .iInterface = USB_STRING_UART,
-    },
-    .CDC_out_endpoint = {
-        .bLength = sizeof(USB_EndpointDescriptor),
-        .bDescriptorType = USB_DTYPE_Endpoint,
-        .bEndpointAddress = USB_EP_CDC_OUT,
-        .bmAttributes = (USB_EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
-        .wMaxPacketSize = 64,
-        .bInterval = 0x05
-    },
-    .CDC_in_endpoint = {
-        .bLength = sizeof(USB_EndpointDescriptor),
-        .bDescriptorType = USB_DTYPE_Endpoint,
-        .bEndpointAddress = USB_EP_CDC_IN,
-        .bmAttributes = (USB_EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
-        .wMaxPacketSize = 64,
-        .bInterval = 0x05
-    }
+    // .CDC_data_interface = {
+    //     .bLength = sizeof(USB_InterfaceDescriptor),
+    //     .bDescriptorType = USB_DTYPE_Interface,
+    //     .bInterfaceNumber = USB_INTERFACE_CDC_DATA,
+    //     .bAlternateSetting = 0,
+    //     .bNumEndpoints = 2,
+    //     .bInterfaceClass = CDC_INTERFACE_CLASS_DATA,
+    //     .bInterfaceSubClass = 0,
+    //     .bInterfaceProtocol = 0,
+    //     .iInterface = USB_STRING_UART,
+    // },
+    // .CDC_out_endpoint = {
+    //     .bLength = sizeof(USB_EndpointDescriptor),
+    //     .bDescriptorType = USB_DTYPE_Endpoint,
+    //     .bEndpointAddress = USB_EP_CDC_OUT,
+    //     .bmAttributes = (USB_EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+    //     .wMaxPacketSize = 64,
+    //     .bInterval = 0x05
+    // },
+    // .CDC_in_endpoint = {
+    //     .bLength = sizeof(USB_EndpointDescriptor),
+    //     .bDescriptorType = USB_DTYPE_Endpoint,
+    //     .bEndpointAddress = USB_EP_CDC_IN,
+    //     .bmAttributes = (USB_EP_TYPE_BULK | ENDPOINT_ATTR_NO_SYNC | ENDPOINT_USAGE_DATA),
+    //     .wMaxPacketSize = 64,
+    //     .bInterval = 0x05
+    // }
 };
 
 static const language_string_t language_string = {
@@ -265,52 +268,55 @@ static void send_descriptor(const void * descriptor, uint16_t length)
 
 
 // ****************************************************************************
-static void usbserial_init(void)
-{
-    usb_enable_ep(USB_EP_CDC_NOTIFICATION, USB_EP_TYPE_INTERRUPT, 8);
-    usb_enable_ep(USB_EP_CDC_OUT, USB_EP_TYPE_BULK, 64);
-    usb_enable_ep(USB_EP_CDC_IN, USB_EP_TYPE_BULK, 64);
+// static void usbserial_init(void)
+// {
+//     usb_enable_ep(USB_EP_CDC_NOTIFICATION, USB_EP_TYPE_INTERRUPT, 8);
+//     usb_enable_ep(USB_EP_CDC_OUT, USB_EP_TYPE_BULK, 64);
+//     usb_enable_ep(USB_EP_CDC_IN, USB_EP_TYPE_BULK, 64);
 
-    usb_ep_start_out(USB_EP_CDC_OUT, usbserial_buf_out, BUF_SIZE);
-}
+//     usb_ep_start_out(USB_EP_CDC_OUT, usbserial_buf_out, BUF_SIZE);
+// }
 
 
-// ****************************************************************************
-static void usbserial_out_completion(void)
-{
-    uint32_t length = usb_ep_out_length(USB_EP_CDC_OUT);
+// // ****************************************************************************
+// static void usbserial_out_completion(void)
+// {
+//     uint32_t length = usb_ep_out_length(USB_EP_CDC_OUT);
 
-    for (uint32_t i = 0; i < length ; i++) {
-        add_uint8_to_receive_buffer(usbserial_buf_out[i]);
-    }
+//     for (uint32_t i = 0; i < length ; i++) {
+//         add_uint8_to_receive_buffer(usbserial_buf_out[i]);
+//     }
 
-    usb_ep_start_out(USB_EP_CDC_OUT, usbserial_buf_out, BUF_SIZE);
-}
+//     usb_ep_start_out(USB_EP_CDC_OUT, usbserial_buf_out, BUF_SIZE);
+// }
 
 // ****************************************************************************
 bool usbserial_is_write_busy(void)
 {
-    return usbserial_buf_in_busy;
+    // return usbserial_buf_in_busy;
+    return false;
 }
 
 // ****************************************************************************
 void usbserial_write(uint8_t *data_to_send, uint8_t length)
 {
-    if (length) {
-        usbserial_buf_in_busy = true;
+    (void) data_to_send;
+    (void) length;
+    // if (length) {
+    //     usbserial_buf_in_busy = true;
 
-        for (uint32_t i = 0; i < length ; i++) {
-            usbserial_buf_in[i] = data_to_send[i];
-        }
-        usb_ep_start_in(USB_EP_CDC_IN, usbserial_buf_in, length, false);
-    }
+    //     for (uint32_t i = 0; i < length ; i++) {
+    //         usbserial_buf_in[i] = data_to_send[i];
+    //     }
+    //     usb_ep_start_in(USB_EP_CDC_IN, usbserial_buf_in, length, false);
+    // }
 }
 
-// ****************************************************************************
-static void usbserial_in_completion(void)
-{
-    usbserial_buf_in_busy = false;
-}
+// // ****************************************************************************
+// static void usbserial_in_completion(void)
+// {
+//     usbserial_buf_in_busy = false;
+// }
 
 
 // ****************************************************************************
@@ -366,12 +372,13 @@ uint16_t usb_cb_get_descriptor(uint8_t type, uint8_t index, const uint8_t** ptr)
                     address = usb_string_to_descriptor((char *)"RC Light Controller (DFU)");
                     break;
 
-                case USB_STRING_UART:
-                    address = usb_string_to_descriptor((char *)"RC Light Controller (UART)");
-                    break;
+                // case USB_STRING_UART:
+                //     address = usb_string_to_descriptor((char *)"RC Light Controller (UART)");
+                //     break;
 
                 default:
-                    break;
+                    *ptr = NULL;
+                    return 0;
             }
             size = (((USB_StringDescriptor *)address))->bLength;
             break;
@@ -392,18 +399,34 @@ uint16_t usb_cb_get_descriptor(uint8_t type, uint8_t index, const uint8_t** ptr)
 
 // ****************************************************************************
 void usb_cb_reset(void) {
-    usbserial_buf_in_busy = false;
+    // usbserial_buf_in_busy = false;
 }
 
 
 // ****************************************************************************
 bool usb_cb_set_configuration(uint8_t config) {
     if (config <= 1) {
-        usbserial_init();
+        // usbserial_init();
         return true;
     }
     return false;
 }
+
+
+// ****************************************************************************
+bool usb_cb_set_interface(uint16_t interface, uint16_t new_altsetting) {
+    (void) new_altsetting;
+
+    switch (interface) {
+        // case USB_INTERFACE_CDC_CONTROL:
+        case USB_INTERFACE_DFU:
+            return true;
+
+        default:
+            return false;
+    }
+}
+
 
 // ****************************************************************************
 void usb_cb_control_setup(void) {
@@ -425,7 +448,7 @@ void usb_cb_control_setup(void) {
                     return;
 
                 default:
-                    return;
+                    break;
             }
         }
     }
@@ -441,7 +464,7 @@ void usb_cb_control_setup(void) {
 
             case VENDOR_CODE_MS:
                 if (usb_setup.wIndex == WINUSB_REQUEST_DESCRIPTOR) {
-                   send_descriptor(&ms_os_20_descriptor, sizeof(ms_os_20_descriptor_t));
+                    send_descriptor(&ms_os_20_descriptor, sizeof(ms_os_20_descriptor_t));
                     return;
                 }
                 break;
@@ -462,7 +485,11 @@ void usb_cb_control_setup(void) {
 
 // ****************************************************************************
 void usb_cb_control_in_completion(void) {
-    send_descriptor_multi();
+    uint8_t recipient = usb_setup.bmRequestType & USB_REQTYPE_RECIPIENT_MASK;
+
+    if (recipient == USB_RECIPIENT_DEVICE) {
+        send_descriptor_multi();
+    }
 }
 
 
@@ -483,35 +510,19 @@ void usb_cb_control_out_completion(void) {
             usb_ep0_in(0);
         }
     }
-
 }
 
 
 // ****************************************************************************
 void usb_cb_completion(void) {
-    if (usb_ep_pending(USB_EP_CDC_OUT)) {
-        usbserial_out_completion();
-        usb_ep_handled(USB_EP_CDC_OUT);
-    }
+    // if (usb_ep_pending(USB_EP_CDC_OUT)) {
+    //     usbserial_out_completion();
+    //     usb_ep_handled(USB_EP_CDC_OUT);
+    // }
 
-    if (usb_ep_pending(USB_EP_CDC_IN)) {
-        usbserial_in_completion();
-        usb_ep_handled(USB_EP_CDC_IN);
-    }
-}
-
-
-// ****************************************************************************
-bool usb_cb_set_interface(uint16_t interface, uint16_t new_altsetting) {
-    (void) new_altsetting;
-
-    switch (interface) {
-        case USB_INTERFACE_CDC_CONTROL:
-        case USB_INTERFACE_DFU:
-            return true;
-
-        default:
-            return false;
-    }
+    // if (usb_ep_pending(USB_EP_CDC_IN)) {
+    //     usbserial_in_completion();
+    //     usb_ep_handled(USB_EP_CDC_IN);
+    // }
 }
 
