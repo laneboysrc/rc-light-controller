fc94daa23c9fe545e4afb64e410b3d4d15707d1f
Werner Lane <laneboysrc@gmail.com>
2018-07-12 11:35:26 +0800

    Add downloading of HTML from Github

--- mk4-tlc5940-lpc812/tools/configurator/src/main-nwjs.js
+++ mk4-tlc5940-lpc812/tools/configurator/src/main-nwjs.js
@@ -1,6 +1,35 @@
 'use strict';
 
 var gui = require('nw.gui');
+var https = require('https');
+
+function getFromGithub(console) {
+    https.get('https://laneboysrc.github.io/rc-light-controller/', (res) => {
+        const { statusCode } = res;
+        // const contentType = res.headers['content-type'];
+
+        let error;
+        if (statusCode !== 200) {
+            error = new Error(`Request Failed Status Code: ${statusCode}`);
+        }
+
+        if (error) {
+            console.log(error.message);
+            // Consume response data to free up memory
+            res.resume();
+            return;
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+            console.log(rawData);
+        });
+    }).on('error', (e) => {
+        console.log(`Got error: ${e.message}`);
+    });
+}
 
 function startConfigurator() {
     const windowOptions = {
@@ -29,6 +58,9 @@ function startConfigurator() {
             gui.Shell.openExternal(url);
             policy.ignore();
         });
+
+
+        getFromGithub(appWindow.contentWindow.console);
     });
 
 
